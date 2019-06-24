99561b3999b5b6e1c0bec38e8fc5d22c4d36bb96
Rémi Pépin <remi.pepin@insee.fr>
2019-06-24 13:51:00 +0200

    rebase for merge request

--- arc_essnet-core/src/main/java/fr/insee/arc_essnet/core/Main.java
+++ arc_essnet-core/src/main/java/fr/insee/arc_essnet/core/Main.java
@@ -1,9 +1,18 @@
 package fr.insee.arc_essnet.core;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import fr.insee.arc_essnet.utils.ressourceUtils.PropertiesHandler;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println(new Exception("test exception").toString().replace("'", "''").replaceAll("\r", ""));
+	GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("applicationContext.xml");
+
+	PropertiesHandler propertitiesHandler = (PropertiesHandler) ctx.getBean("properties");
+	
+	System.out.println(propertitiesHandler.getAnnuaireArcIdent());
+    
     }
 
 }
