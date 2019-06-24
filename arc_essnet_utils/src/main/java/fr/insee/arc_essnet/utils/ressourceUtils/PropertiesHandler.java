c2cfec079e85fac795bfafe3e2a0563cf8c89d24
Rémi Pépin <remi.pepin@insee.fr>
2019-06-24 13:29:43 +0200

    correction pb des log (imparfait)

--- arc_essnet_utils/src/main/java/fr/insee/arc_essnet/utils/ressourceUtils/PropertiesHandler.java
+++ arc_essnet_utils/src/main/java/fr/insee/arc_essnet/utils/ressourceUtils/PropertiesHandler.java
@@ -1,37 +1,42 @@
 package fr.insee.arc_essnet.utils.ressourceUtils;
 
+import java.net.URL;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Component("properties")
 @Getter
 @Setter
-public class PropertiesHandler {
-    
-    private String databasePoolName ;
-    private String databaseArcUrl ;
-    private String databaseArcUsername ;
-    private String databaseArcPassword ;
-    private String databaseArcDriverClassName ;
-    private String databaseArcSchema ;
-    private String annuaireArcUri ; 
-    private String annuaireArcIdent ;
-    private String annuaireArcPassword ;
-    private String logChemin  ;
-    private String logNiveau ;
-    private String logConfiguration ;
-    private String batchParametreRepertoire ;
-    private int threadsChargement ;
-    private int threadsNormage ;
-    private int threadsControle ;
-    private int threadsFiltrage ;
-    private int threadsMapping ;
-    private int threadsRegle ;
-    private int  threadNombre;
+public class PropertiesHandler implements ApplicationListener<ContextStartedEvent> {
+
+    private String databasePoolName;
+    private String databaseArcUrl;
+    private String databaseArcUsername;
+    private String databaseArcPassword;
+    private String databaseArcDriverClassName;
+    private String databaseArcSchema;
+    private String annuaireArcUri;
+    private String annuaireArcIdent;
+    private String annuaireArcPassword;
+    private String logChemin;
+    private String logNiveau;
+    private String logConfiguration;
+    private String batchParametreRepertoire;
+    private int threadsChargement;
+    private int threadsNormage;
+    private int threadsControle;
+    private int threadsFiltrage;
+    private int threadsMapping;
+    private int threadsRegle;
+    private int threadNombre;
     private String version;
     private String schemaReference;
     private Boolean isProd;
@@ -41,7 +46,16 @@ public class PropertiesHandler {
     private String repertoireReception;
     private String repertoireChargement;
     private String repertoireStockage;
-    
+
+    public void initializeLog() {
+	System.out.println("plop");
+	URL log4jprops = this.getClass().getClassLoader().getResource(logConfiguration);
+	// si la ressource est une ressource interne au projet : du type
+	// "fr/insee/config/log4j.xml"
+	DOMConfigurator.configure(log4jprops);
+
+    }
+
     public static PropertiesHandler getInstance() {
 	GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("applicationContext.xml");
 
@@ -50,6 +64,10 @@ public class PropertiesHandler {
 	return propertitiesHandler;
     }
 
-
+    @Override
+    public void onApplicationEvent(ContextStartedEvent event) {
+	initializeLog();
+	
+    }
 
 }
