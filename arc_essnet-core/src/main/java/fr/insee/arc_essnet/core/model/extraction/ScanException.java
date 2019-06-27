e937083a8d21b4031a2506d9368e0aec60dc8fb5
Manuel Soulier <manuel.soulier@insee.fr>
2019-06-27 23:23:37 +0200

    Removed the "extraction" functions with the agreement of our main user as it was developed for some very particular files and not in a generic way

--- arc_essnet-core/src/main/java/fr/insee/arc_essnet/core/model/extraction/ScanException.java
+++
@@ -1,8 +0,0 @@
-package fr.insee.arc_essnet.core.model.extraction;
-
-public class ScanException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-	public ScanException(String message){super(message);}
-}
