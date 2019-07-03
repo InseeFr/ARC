a068a03a698f146f8d512847923567a5f5697984
Rémi Pépin <remi.pepin@insee.fr>
2019-07-03 13:48:37 +0200

    fix properties problem

--- arc-utils/src/main/java/fr/insee/arc/utils/ressourceUtils/PropertiesHandler.java
+++ arc-utils/src/main/java/fr/insee/arc/utils/ressourceUtils/PropertiesHandler.java
@@ -1,20 +1,12 @@
 package fr.insee.arc.utils.ressourceUtils;
 
-
-import java.net.URL;
-
-import org.apache.log4j.xml.DOMConfigurator;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 @Component("properties")
-@Getter
-@Setter
-//public class PropertiesHandler implements ApplicationListener<ContextStartedEvent> {
-	public class PropertiesHandler {
+public class PropertiesHandler {
 
     private String databasePoolName;
     private String databaseArcUrl;
@@ -49,15 +41,6 @@ import lombok.Setter;
     private String repertoireStockage;
 
 
-    public void initializeLog() {
-	URL log4jprops = this.getClass().getClassLoader().getResource(logConfiguration);
-	// si la ressource est une ressource interne au projet : du type
-	// "fr/insee/config/log4j.xml"
-	DOMConfigurator.configure(log4jprops);
-
-    }
-
-
     public static PropertiesHandler getInstance() {
 	GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("applicationContext.xml");
 
@@ -66,325 +49,316 @@ import lombok.Setter;
 	return propertitiesHandler;
     }
 
-//    @Override
-//    public void onApplicationEvent(ContextStartedEvent event) {
-//	initializeLog();
-//	
-//    }
 
+    public String getDatabasePoolName() {
+        return databasePoolName;
+    }
 
-	public String getDatabasePoolName() {
-		return databasePoolName;
-	}
 
+    public void setDatabasePoolName(String databasePoolName) {
+        this.databasePoolName = databasePoolName;
+    }
 
-	public void setDatabasePoolName(String databasePoolName) {
-		this.databasePoolName = databasePoolName;
-	}
 
+    public String getDatabaseArcUrl() {
+        return databaseArcUrl;
+    }
 
-	public String getDatabaseArcUrl() {
-		return databaseArcUrl;
-	}
 
+    public void setDatabaseArcUrl(String databaseArcUrl) {
+        this.databaseArcUrl = databaseArcUrl;
+    }
 
-	public void setDatabaseArcUrl(String databaseArcUrl) {
-		this.databaseArcUrl = databaseArcUrl;
-	}
 
+    public String getDatabaseArcUsername() {
+        return databaseArcUsername;
+    }
 
-	public String getDatabaseArcUsername() {
-		return databaseArcUsername;
-	}
 
+    public void setDatabaseArcUsername(String databaseArcUsername) {
+        this.databaseArcUsername = databaseArcUsername;
+    }
 
-	public void setDatabaseArcUsername(String databaseArcUsername) {
-		this.databaseArcUsername = databaseArcUsername;
-	}
 
+    public String getDatabaseArcPassword() {
+        return databaseArcPassword;
+    }
 
-	public String getDatabaseArcPassword() {
-		return databaseArcPassword;
-	}
 
+    public void setDatabaseArcPassword(String databaseArcPassword) {
+        this.databaseArcPassword = databaseArcPassword;
+    }
 
-	public void setDatabaseArcPassword(String databaseArcPassword) {
-		this.databaseArcPassword = databaseArcPassword;
-	}
 
+    public String getDatabaseArcDriverClassName() {
+        return databaseArcDriverClassName;
+    }
 
-	public String getDatabaseArcDriverClassName() {
-		return databaseArcDriverClassName;
-	}
 
+    public void setDatabaseArcDriverClassName(String databaseArcDriverClassName) {
+        this.databaseArcDriverClassName = databaseArcDriverClassName;
+    }
 
-	public void setDatabaseArcDriverClassName(String databaseArcDriverClassName) {
-		this.databaseArcDriverClassName = databaseArcDriverClassName;
-	}
 
+    public String getDatabaseArcSchema() {
+        return databaseArcSchema;
+    }
 
-	public String getDatabaseArcSchema() {
-		return databaseArcSchema;
-	}
 
+    public void setDatabaseArcSchema(String databaseArcSchema) {
+        this.databaseArcSchema = databaseArcSchema;
+    }
 
-	public void setDatabaseArcSchema(String databaseArcSchema) {
-		this.databaseArcSchema = databaseArcSchema;
-	}
 
+    public String getAnnuaireArcUri() {
+        return annuaireArcUri;
+    }
 
-	public String getAnnuaireArcUri() {
-		return annuaireArcUri;
-	}
 
+    public void setAnnuaireArcUri(String annuaireArcUri) {
+        this.annuaireArcUri = annuaireArcUri;
+    }
 
-	public void setAnnuaireArcUri(String annuaireArcUri) {
-		this.annuaireArcUri = annuaireArcUri;
-	}
 
+    public String getAnnuaireArcIdent() {
+        return annuaireArcIdent;
+    }
 
-	public String getAnnuaireArcIdent() {
-		return annuaireArcIdent;
-	}
 
+    public void setAnnuaireArcIdent(String annuaireArcIdent) {
+        this.annuaireArcIdent = annuaireArcIdent;
+    }
 
-	public void setAnnuaireArcIdent(String annuaireArcIdent) {
-		this.annuaireArcIdent = annuaireArcIdent;
-	}
 
+    public String getAnnuaireArcPassword() {
+        return annuaireArcPassword;
+    }
 
-	public String getAnnuaireArcPassword() {
-		return annuaireArcPassword;
-	}
 
+    public void setAnnuaireArcPassword(String annuaireArcPassword) {
+        this.annuaireArcPassword = annuaireArcPassword;
+    }
 
-	public void setAnnuaireArcPassword(String annuaireArcPassword) {
-		this.annuaireArcPassword = annuaireArcPassword;
-	}
 
+    public String getLogChemin() {
+        return logChemin;
+    }
 
-	public String getLogChemin() {
-		return logChemin;
-	}
 
+    public void setLogChemin(String logChemin) {
+        this.logChemin = logChemin;
+    }
 
-	public void setLogChemin(String logChemin) {
-		this.logChemin = logChemin;
-	}
 
+    public String getLogNiveau() {
+        return logNiveau;
+    }
 
-	public String getLogNiveau() {
-		return logNiveau;
-	}
 
+    public void setLogNiveau(String logNiveau) {
+        this.logNiveau = logNiveau;
+    }
 
-	public void setLogNiveau(String logNiveau) {
-		this.logNiveau = logNiveau;
-	}
 
+    public String getLogConfiguration() {
+        return logConfiguration;
+    }
 
-	public String getLogConfiguration() {
-		return logConfiguration;
-	}
 
+    public void setLogConfiguration(String logConfiguration) {
+        this.logConfiguration = logConfiguration;
+    }
 
-	public void setLogConfiguration(String logConfiguration) {
-		this.logConfiguration = logConfiguration;
-		// TODO use spring Log4jConfigListener ?
-		initializeLog();
-	}
 
+    public String getBatchParametreRepertoire() {
+        return batchParametreRepertoire;
+    }
 
-	public String getBatchParametreRepertoire() {
-		return batchParametreRepertoire;
-	}
 
+    public void setBatchParametreRepertoire(String batchParametreRepertoire) {
+        this.batchParametreRepertoire = batchParametreRepertoire;
+    }
 
-	public void setBatchParametreRepertoire(String batchParametreRepertoire) {
-		this.batchParametreRepertoire = batchParametreRepertoire;
-	}
-	
-	
-	public String getBatchExecutionEnvironment() {
-		return batchExecutionEnvironment;
-	}
 
+    public String getBatchExecutionEnvironment() {
+        return batchExecutionEnvironment;
+    }
 
-	public void setBatchExecutionEnvironment(String batchExecutionEnvironment) {
-		this.batchExecutionEnvironment = batchExecutionEnvironment;
-	}
 
+    public void setBatchExecutionEnvironment(String batchExecutionEnvironment) {
+        this.batchExecutionEnvironment = batchExecutionEnvironment;
+    }
 
-	public String getBatchArcEnvironment() {
-		return batchArcEnvironment;
-	}
 
+    public String getBatchArcEnvironment() {
+        return batchArcEnvironment;
+    }
 
-	public void setBatchArcEnvironment(String batchArcEnvironment) {
-		this.batchArcEnvironment = batchArcEnvironment;
-	}
 
+    public void setBatchArcEnvironment(String batchArcEnvironment) {
+        this.batchArcEnvironment = batchArcEnvironment;
+    }
 
-	public int getThreadsChargement() {
-		return threadsChargement;
-	}
 
+    public int getThreadsChargement() {
+        return threadsChargement;
+    }
 
-	public void setThreadsChargement(int threadsChargement) {
-		this.threadsChargement = threadsChargement;
-	}
 
+    public void setThreadsChargement(int threadsChargement) {
+        this.threadsChargement = threadsChargement;
+    }
 
-	public int getThreadsNormage() {
-		return threadsNormage;
-	}
 
+    public int getThreadsNormage() {
+        return threadsNormage;
+    }
 
-	public void setThreadsNormage(int threadsNormage) {
-		this.threadsNormage = threadsNormage;
-	}
 
+    public void setThreadsNormage(int threadsNormage) {
+        this.threadsNormage = threadsNormage;
+    }
 
-	public int getThreadsControle() {
-		return threadsControle;
-	}
 
+    public int getThreadsControle() {
+        return threadsControle;
+    }
 
-	public void setThreadsControle(int threadsControle) {
-		this.threadsControle = threadsControle;
-	}
 
+    public void setThreadsControle(int threadsControle) {
+        this.threadsControle = threadsControle;
+    }
 
-	public int getThreadsFiltrage() {
-		return threadsFiltrage;
-	}
 
+    public int getThreadsFiltrage() {
+        return threadsFiltrage;
+    }
 
-	public void setThreadsFiltrage(int threadsFiltrage) {
-		this.threadsFiltrage = threadsFiltrage;
-	}
 
+    public void setThreadsFiltrage(int threadsFiltrage) {
+        this.threadsFiltrage = threadsFiltrage;
+    }
 
-	public int getThreadsMapping() {
-		return threadsMapping;
-	}
 
+    public int getThreadsMapping() {
+        return threadsMapping;
+    }
 
-	public void setThreadsMapping(int threadsMapping) {
-		this.threadsMapping = threadsMapping;
-	}
 
+    public void setThreadsMapping(int threadsMapping) {
+        this.threadsMapping = threadsMapping;
+    }
 
-	public int getThreadsRegle() {
-		return threadsRegle;
-	}
 
+    public int getThreadsRegle() {
+        return threadsRegle;
+    }
 
-	public void setThreadsRegle(int threadsRegle) {
-		this.threadsRegle = threadsRegle;
-	}
 
+    public void setThreadsRegle(int threadsRegle) {
+        this.threadsRegle = threadsRegle;
+    }
 
-	public int getThreadNombre() {
-		return threadNombre;
-	}
 
+    public int getThreadNombre() {
+        return threadNombre;
+    }
 
-	public void setThreadNombre(int threadNombre) {
-		this.threadNombre = threadNombre;
-	}
 
+    public void setThreadNombre(int threadNombre) {
+        this.threadNombre = threadNombre;
+    }
 
-	public String getVersion() {
-		return version;
-	}
 
+    public String getVersion() {
+        return version;
+    }
 
-	public void setVersion(String version) {
-		this.version = version;
-	}
 
+    public void setVersion(String version) {
+        this.version = version;
+    }
 
-	public String getSchemaReference() {
-		return schemaReference;
-	}
 
+    public String getSchemaReference() {
+        return schemaReference;
+    }
 
-	public void setSchemaReference(String schemaReference) {
-		this.schemaReference = schemaReference;
-	}
 
+    public void setSchemaReference(String schemaReference) {
+        this.schemaReference = schemaReference;
+    }
 
-	public Boolean getIsProd() {
-		return isProd;
-	}
 
+    public Boolean getIsProd() {
+        return isProd;
+    }
 
-	public void setIsProd(Boolean isProd) {
-		this.isProd = isProd;
-	}
 
+    public void setIsProd(Boolean isProd) {
+        this.isProd = isProd;
+    }
 
-	public String getApplication() {
-		return application;
-	}
 
+    public String getApplication() {
+        return application;
+    }
 
-	public void setApplication(String application) {
-		this.application = application;
-	}
 
+    public void setApplication(String application) {
+        this.application = application;
+    }
 
-	public String getTn() {
-		return tn;
-	}
 
+    public String getTn() {
+        return tn;
+    }
 
-	public void setTn(String tn) {
-		this.tn = tn;
-	}
 
+    public void setTn(String tn) {
+        this.tn = tn;
+    }
 
-	public String getRepertoireRoot() {
-		return repertoireRoot;
-	}
 
+    public String getRepertoireRoot() {
+        return repertoireRoot;
+    }
 
-	public void setRepertoireRoot(String repertoireRoot) {
-		this.repertoireRoot = repertoireRoot;
-	}
 
+    public void setRepertoireRoot(String repertoireRoot) {
+        this.repertoireRoot = repertoireRoot;
+    }
 
-	public String getRepertoireReception() {
-		return repertoireReception;
-	}
 
+    public String getRepertoireReception() {
+        return repertoireReception;
+    }
 
-	public void setRepertoireReception(String repertoireReception) {
-		this.repertoireReception = repertoireReception;
-	}
 
+    public void setRepertoireReception(String repertoireReception) {
+        this.repertoireReception = repertoireReception;
+    }
 
-	public String getRepertoireChargement() {
-		return repertoireChargement;
-	}
 
+    public String getRepertoireChargement() {
+        return repertoireChargement;
+    }
 
-	public void setRepertoireChargement(String repertoireChargement) {
-		this.repertoireChargement = repertoireChargement;
-	}
 
+    public void setRepertoireChargement(String repertoireChargement) {
+        this.repertoireChargement = repertoireChargement;
+    }
 
-	public String getRepertoireStockage() {
-		return repertoireStockage;
-	}
 
+    public String getRepertoireStockage() {
+        return repertoireStockage;
+    }
 
-	public void setRepertoireStockage(String repertoireStockage) {
-		this.repertoireStockage = repertoireStockage;
-	}
 
-    
-    
-    
+    public void setRepertoireStockage(String repertoireStockage) {
+        this.repertoireStockage = repertoireStockage;
+    }
+
+
+
 }
