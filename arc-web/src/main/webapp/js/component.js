a7295eb843a074df56422d07b114cb83bb6f0d0b
Pierre Le Corre <pierre.le-corre@insee.fr>
2021-02-04 16:11:44 +0100

    Fix being able to change page size on Enter

--- arc-web/src/main/webapp/js/component.js
+++ arc-web/src/main/webapp/js/component.js
@@ -247,6 +247,8 @@ $(document).on('ready readyAgain',function() {
 		$("textarea").filter("[name*='.inputFields']").off('keydown').on('keydown',function(event){addCells(event,$(this));});
 		$("textarea").filter("[name*='.filterFields']").off('keydown').on('keydown',function(event){updateSelect(event,$(this));});
 		$("input").filter("[name*='.idPage']").off('keydown').on('keydown',function(event){updateSelect(event,$(this));});
+		$("input").filter("[name*='.paginationSize']").off('keydown').on('keydown',function(event){updateSelect(event,$(this));});
+		
 
 		jQuery(document).off('keydown').on('keydown',function(evt) {
 			if (evt.keyCode == 27) {
