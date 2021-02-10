09928a2ab082f9c086b552e484ce4e5b7d3af591
MS <MS@MS-PC>
2021-02-10 04:04:49 +0100

    temporary fix for multithread & autowired loggerdispatcher

--- arc-core/src/main/java/fr/insee/arc/core/util/StaticLoggerDispatcher.java
+++ arc-core/src/main/java/fr/insee/arc/core/util/StaticLoggerDispatcher.java
@@ -5,64 +5,77 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import fr.insee.arc.utils.utils.LoggerHelper;
+
 /** Transition class. Try to use a LoggerDispatcher as an @Autowired attribute instead. */
 @Component
 public class StaticLoggerDispatcher {
 	
-	private static LoggerDispatcher loggerDispatcher;
-	
-	public StaticLoggerDispatcher(@Autowired @Qualifier("activeLoggerDispatcher") LoggerDispatcher loggerDispatcherInstance) {
-		loggerDispatcher = loggerDispatcherInstance;
-	}
+//	private static LoggerDispatcher loggerDispatcher;
+//	
+//	public StaticLoggerDispatcher(@Autowired @Qualifier("activeLoggerDispatcher") LoggerDispatcher loggerDispatcherInstance) {
+//		loggerDispatcher = loggerDispatcherInstance;
+//	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void error(Object message, Logger logger) {
-		loggerDispatcher.error(message, logger);		
+//		loggerDispatcher.error(message, logger);
+		LoggerHelper.error(logger,message);
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void error(Object message, Exception ex, Logger logger) {
-		loggerDispatcher.error(message, ex, logger);
+//		loggerDispatcher.error(message, ex, logger);
+		LoggerHelper.error(logger,message,ex);
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void warn(Object message, Logger logger) {
-		loggerDispatcher.warn(message, logger);		
+//		loggerDispatcher.warn(message, logger);
+		LoggerHelper.warn(logger,message);
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void warn(Object message, Exception ex, Logger logger) {
-		loggerDispatcher.warn(message, ex, logger);
+//		loggerDispatcher.warn(message, ex, logger);
+		LoggerHelper.warn(logger,message,ex);
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void info(Object message, Logger logger) {
-		loggerDispatcher.info(message, logger);
+//		loggerDispatcher.info(message, logger);
+		LoggerHelper.info(logger,message);
+
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void info(Object message, Exception ex, Logger logger) {
-		loggerDispatcher.info(message, ex, logger);
+//		loggerDispatcher.info(message, ex, logger);
+		LoggerHelper.info(logger,message,ex);
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void debug(Object message, Logger logger) {
-		loggerDispatcher.debug(message, logger);
+//		loggerDispatcher.debug(message, logger);
+		LoggerHelper.debug(logger,message);
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void debug(Object message, Exception ex, Logger logger) {
-		loggerDispatcher.debug(message, ex, logger);
+//		loggerDispatcher.debug(message, ex, logger);
+		LoggerHelper.debug(logger,message,ex);
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void trace(Object message, Logger logger) {
-		loggerDispatcher.trace(message, logger);
+//		loggerDispatcher.trace(message, logger);
+		LoggerHelper.trace(logger,message);
 	}
 
 	/** Transition class. Use a LoggerDispatcher as an @Autowired attribute instead. */
 	public static void trace(Object message, Exception ex, Logger logger) {
-		loggerDispatcher.trace(message, ex, logger);
+//		loggerDispatcher.trace(message, ex, logger);
+		LoggerHelper.trace(logger,message,ex);
 	}
 	
 }
