3e9001ad6f375451c8129e606687877413aea787
Pierre Le Corre <pierre.le-corre@insee.fr>
2021-02-15 08:59:56 +0100

    Adds table name to mapping ruleset for user info

--- arc-web/src/main/java/fr/insee/arc/web/model/viewobjects/ViewMapping.java
+++ arc-web/src/main/java/fr/insee/arc/web/model/viewobjects/ViewMapping.java
@@ -14,9 +14,10 @@ public class ViewMapping extends AbstractViewRule {
 	protected Map<String, ColumnRendering> extraFields() {
 		HashMap<String, ColumnRendering> columnRenderings = new HashMap<>();
         columnRenderings.put("variable_sortie", new ColumnRendering(true, "label.mapmodel.field", "10%", "text", null, false));
-        columnRenderings.put("expr_regle_col", new ColumnRendering(true, "label.sql.expression", "48%", "text", null, true));
+        columnRenderings.put("expr_regle_col", new ColumnRendering(true, "label.sql.expression", "40%", "text", null, true));
         columnRenderings.put("type_sortie", new ColumnRendering(true, "label.mapmodel.field.type", "10%", "text", null, false));
-        columnRenderings.put("type_consolidation", new ColumnRendering(false, "label.mapmodel.field.aggregate", "17%", "text", null, false));
+        columnRenderings.put("type_consolidation", new ColumnRendering(false, "label.mapmodel.field.aggregate", "15%", "text", null, false));
+        columnRenderings.put("nom_table_metier", new ColumnRendering(true, "Table métier", "15%", "text", null, false));
         columnRenderings.put("commentaire", new ColumnRendering(true, "label.comment", "10%", "text", null, true));
 		return columnRenderings;
 	}
