3676d894f8bb88cfa6dfb325fa607025e7bf69ee
Pierre Le Corre <pierre.le-corre@insee.fr>
2021-04-15 14:50:00 +0200

    Adds some unit tests

--- arc-utils/src/test/java/fr/insee/arc/utils/structure/GenericBeanTest.java
+++ arc-utils/src/test/java/fr/insee/arc/utils/structure/GenericBeanTest.java
@@ -2,14 +2,105 @@ package fr.insee.arc.utils.structure;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class GenericBeanTest {
 
+	private GenericBean genericBean;
+	/** Defined by default as {"col1", "col2"}*/
+	private ArrayList<String> headers;
+	/** Defined by default as {"text", "text"}*/
+	private ArrayList<String> types;
+	
+	@Before
+	public void setUp() {
+		headers = new ArrayList<>();
+		headers.add("col1");
+		headers.add("col2");
+		types = new ArrayList<>();
+		types.add("text");
+		types.add("text");
+	}
+	
 	@Test
-	public void getHeadersUpperCase() {
-		 assertEquals(1, 1);
+	public void mapIndexOk() {
+		genericBean = new GenericBean(headers, types, new ArrayList<ArrayList<String>>());
+		HashMap<String, Integer> mapIndex = genericBean.mapIndex();
+		assertEquals(0, mapIndex.get("col1").intValue());
+		assertEquals(1, mapIndex.get("col2").intValue());
+	}
+
+	@Test
+	public void mapTypes() {
+		//with
+		types = new ArrayList<>();
+		types.add("text");
+		types.add("int");
+		//when
+		genericBean = new GenericBean(headers, types, new ArrayList<ArrayList<String>>());
+		HashMap<String, String> mapTypes = genericBean.mapTypes();
+		//then
+		assertEquals("text", mapTypes.get("col1"));
+		assertEquals("int", mapTypes.get("col2"));
+	}
+	
+	@Test
+	public void mapContentOk() {
+		//with
+		ArrayList<ArrayList<String>> content = new ArrayList<ArrayList<String>>();
+		content.add(new ArrayList<>());
+		content.get(0).add("col1_val1");
+		content.get(0).add("col2_val1");
+		content.add(new ArrayList<>());
+		content.get(1).add("col1_val2");
+		content.get(1).add("col2_val2");
+		//when
+		genericBean = new GenericBean(headers, types, content);
+		HashMap<String, ArrayList<String>> mapContent = genericBean.mapContent();
+		//then
+		assertEquals("col1_val1", mapContent.get("col1").get(0));
+		assertEquals("col1_val2", mapContent.get("col1").get(1));
+		assertEquals("col2_val1", mapContent.get("col2").get(0));
+		assertEquals("col2_val2", mapContent.get("col2").get(1));
+	}
+
+	@Test
+	public void mapContentEmpty() {
+		genericBean = new GenericBean(headers, types, new ArrayList<ArrayList<String>>());
+		assertTrue(genericBean.mapContent().isEmpty());
+	}
+	
+	@Test
+	public void empty() {
+		//with
+		genericBean = new GenericBean(headers, types, new ArrayList<ArrayList<String>>());
+		//then
+		assertTrue(genericBean.isEmpty());
+	}
+	
+	@Test
+	public void notEmpty() {
+		//with
+		ArrayList<ArrayList<String>> content = new ArrayList<ArrayList<String>>();
+		content.add(new ArrayList<>());
+		content.get(0).add("col1_val1");
+		content.get(0).add("col2_val1");
+		genericBean = new GenericBean(headers, types, content);
+		//then
+		assertTrue(!genericBean.isEmpty());
+	}
+	
+	@Test
+	public void getHeadersUpperCaseOk() {
+		genericBean = new GenericBean(headers, types, new ArrayList<ArrayList<String>>());
+		assertEquals("COL1", genericBean.getHeadersUpperCase().get(0));
+		assertEquals("COL2", genericBean.getHeadersUpperCase().get(1));
 	}
 
 }
--- arc-utils/src/test/java/fr/insee/arc/utils/utils/ManipStringTest.java
+++ arc-utils/src/test/java/fr/insee/arc/utils/utils/ManipStringTest.java
@@ -20,5 +20,93 @@ public class ManipStringTest {
 	public void isStringNullNo() {
 		assertEquals(false, ManipString.isStringNull("hi"));
 	}
-	
+
+	// substringBeforeFirst
+	@Test
+	public void substringBeforeFirstNullString() {
+		assertEquals(null, ManipString.substringBeforeFirst(null, "-"));
+	}
+
+
+	@Test
+	public void substringBeforeFirstNullSep() {
+		assertEquals("abc", ManipString.substringBeforeFirst("abc", null));
+	}
+
+	@Test
+	public void substringBeforeFirstNoSep() {
+		assertEquals("abc", ManipString.substringBeforeFirst("abc", "-"));
+	}
+
+	@Test
+	public void substringBeforeFirstOk() {
+		assertEquals("a", ManipString.substringBeforeFirst("a-b-c", "-"));
+	}
+
+	// substringBeforeLast
+	@Test
+	public void substringBeforeLastNullString() {
+		assertEquals(null, ManipString.substringBeforeLast(null, "-"));
+	}
+
+
+	@Test
+	public void substringBeforeLastNullSep() {
+		assertEquals("abc", ManipString.substringBeforeLast("abc", null));
+	}
+
+	@Test
+	public void substringBeforeLastNoSep() {
+		assertEquals("abc", ManipString.substringBeforeLast("abc", "-"));
+	}
+
+	@Test
+	public void substringBeforeLastOk() {
+		assertEquals("a-b", ManipString.substringBeforeLast("a-b-c", "-"));
+	}
+
+	// substringAfterFirst
+	@Test
+	public void substringAfterFirstNullString() {
+		assertEquals(null, ManipString.substringAfterFirst(null, "-"));
+	}
+
+
+	@Test
+	public void substringAfterFirstNullSep() {
+		assertEquals("abc", ManipString.substringAfterFirst("abc", null));
+	}
+
+	@Test
+	public void substringAfterFirstNoSep() {
+		assertEquals("abc", ManipString.substringAfterFirst("abc", "-"));
+	}
+
+	@Test
+	public void substringAfterFirstOk() {
+		assertEquals("b-c", ManipString.substringAfterFirst("a-b-c", "-"));
+	}
+
+	// substringAfterLast
+	@Test
+	public void substringAfterLastNullString() {
+		assertEquals(null, ManipString.substringAfterLast(null, "-"));
+	}
+
+
+	@Test
+	public void substringAfterLastNullSep() {
+		assertEquals("abc", ManipString.substringAfterLast("abc", null));
+	}
+
+	@Test
+	public void substringAfterLastNoSep() {
+		assertEquals("abc", ManipString.substringAfterLast("abc", "-"));
+	}
+
+	@Test
+	public void substringAfterLastOk() {
+		assertEquals("c", ManipString.substringAfterLast("a-b-c", "-"));
+	}
+
 }
