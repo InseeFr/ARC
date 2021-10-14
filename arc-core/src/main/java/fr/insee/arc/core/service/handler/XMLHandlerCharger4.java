f2ae6a3e32acce4a02d06ffa97fb46cff222e4a4
Manuel Soulier <manuel.soulier@insee.fr>
2021-10-14 15:17:50 +0200

    removed useless comment on xml handler class

--- arc-core/src/main/java/fr/insee/arc/core/service/handler/XMLHandlerCharger4.java
+++ arc-core/src/main/java/fr/insee/arc/core/service/handler/XMLHandlerCharger4.java
@@ -128,7 +128,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 			UtilitaireDao.get("arc").executeImmediate(this.connexion, this.requete);
 		} catch (SQLException ex) {
 			LoggerHelper.errorGenTextAsComment(getClass(), "startElement()", LOGGER, ex);
-			//e.printStackTrace();
 			 throw new SAXParseException("Fichier XML : erreur de requete insertion  : "+ex.getMessage() , "", "", 0, 0);
 		}
 		this.requete.setLength(0);
@@ -151,28 +150,9 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 		// on doit arriver à la fin du stream de l'element et concaténer toutes
 		// les données trouvées jusqu'a ce qu'on en trouve plus
 		if (this.closedTag.equals(this.currentTag) && this.hasData) {
-			// System.out.println(currentTag+" = " + donnees + " ; ");
-
 			if (this.colData.get(this.currentTag) == null) {
 				this.colData.put(this.currentTag, 1);
-				// try {
-				// if (pst!=null){
-				//
-				//
-				// requete=Format.executeBlock(st, requete);
-				//
-				// pst.executeBatch();
-				// pst=null;
-				// }
-
 				this.requete.append("alter table " + this.tempTableA + " add v" + this.allCols.indexOf(this.currentTag) + " " + this.textBdType + ";");
-
-//				this.requete.append("alter table " + this.tempTableA + " add v_" + this.currentTag + " " + this.textBdType + ";");
-
-				// } catch (SQLException e) {
-				// 666 LoggerHelper.errorGenTextAsComment(getClass(), "index()", LOGGER, ex);
-				// e.printStackTrace();
-				// }
 			}
 
 			this.lineValues.remove(this.lineValues.size() - 1);
@@ -183,20 +163,9 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 
 
 		this.distance--;
-		// System.out.println(Arrays.toString(treeStack.toArray()));
-		// System.out.println(Arrays.toString(treeStackFather.toArray()));
-
-		//
-		// System.out.println(Arrays.toString(treeStackFatherLag.toArray()));
-		//
-		// System.out.println(Arrays.toString(treeStackFather.toArray()));
-
-		// modif : updated
-		// updateNeeded.remove(closedTag);
 
 		this.leafStatus = false;
 
-		// treeStackFatherLag= new ArrayList<String>(treeStackFather);
 		this.treeStackFatherLag = new ArrayList<String>(this.treeStackFather);
 
 		this.treeStack.remove(this.treeStack.size() - 1);
@@ -208,21 +177,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 			// vérifier qu'une feuille n'existe pas déjà
 			 if (this.lineCols.indexOf(this.allCols.indexOf(this.closedTag)) < this.lineCols.size() - 1) {
 
-//				 System.out.println();
-//				 System.out.println();
-//				 System.out.println("allCols : "+ allCols);
-//				 System.out.println("lineCols : "+ lineCols);
-//				 System.out.println("lineIds : "+ lineIds);
-//				 System.out.println("lineValues : "+ lineValues);
-//				 System.out.println("lineCols11 : "+ lineCols11);
-//				 System.out.println("treeStack : "+ treeStack);
-//				 System.out.println("treeFatherStack : "+ treeStackFather);
-//				 System.out.println("treeStackFatherLag : "+ treeStackFatherLag);
-//
-//
-//				 System.out.println();
-//				 System.out.println();
-//
 				 insertQueryBuilder(this.requete,this.tempTableA, this.fileName, this.lineCols.subList(0,this.lineCols.size()-1), this.lineIds.subList(0,this.lineCols.size()-1), this.lineValues.subList(0,this.lineCols.size()-1));
 
 				 int i = this.lineCols.indexOf(this.allCols.indexOf(this.closedTag));
@@ -242,35 +196,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 				 String h= this.lineValues.get(this.lineValues.size()-1);
 				 Format.removeToIndex(this.lineValues, i);
 				 this.lineValues.add(h);
-
-
-//				 System.out.println();
-//				 System.out.println();
-//				 System.out.println("allCols : "+ allCols);
-//				 System.out.println("lineCols : "+ lineCols);
-//				 System.out.println("lineIds : "+ lineIds);
-//				 System.out.println("lineValues : "+ lineValues);
-//				 System.out.println("lineCols11 : "+ lineCols11);
-//				 System.out.println();
-//				 System.out.println();
-//
-
-
-
-	//			 throw new SAXParseException("Fichier XML non pris en charge : feuille en doublon de rubrique  : " + closedTag, "", "", 0, 0);
-
-//					insertQueryBuilder(requete,tempTableI, fileName, lineCols, lineIds, lineValues);
-//					int fatherIndex = lineCols11.lastIndexOf(allCols.indexOf(father)) + 1;
-//
-//					//
-//					// System.out.println(father);
-//					// System.out.println(fatherIndex);
-//					// System.out.println(Arrays.toString(lineCols.toArray()));
-//
-//					Format.removeToIndexInt(lineCols11, fatherIndex);
-//					Format.removeToIndexInt(lineCols, fatherIndex);
-//					Format.removeToIndexInt(lineIds, fatherIndex);
-//					Format.removeToIndex(lineValues, fatherIndex);
 
 			 }
 
@@ -294,13 +219,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 		// creation de la table
 
 		try {
-//			this.requete.append("DROP TABLE IF EXISTS " + this.tempTableI + " cascade;");
-//			this.requete.append("CREATE ");
-//			if (!this.tempTableI.contains(".")) {
-//				this.requete.append("TEMPORARY ");
-//			}
-//			this.requete.append(" TABLE " + this.tempTableI + " "+FormatSQL.WITH_NO_VACUUM+" as select * from " + this.tempTableA + " where 1=0;");
-
 			this.colDist.put(-1, 0);
 
 		} catch (Exception ex) {
@@ -319,8 +237,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 		this.currentTag = Format.toBdRaw(qName);
 		this.currentData.setLength(0);
 		this.hasData=false;
-//		this.firstData = true;
-		// distance++;
 
 		// on ajoute les colonnes si besoin
 		// on met à jour le numéro d'index
@@ -330,36 +246,13 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 		// créer et enregistrer la colonne si elle n'existe pas
 		if (o == null) {
 			this.col.put(this.currentTag, 1);
-			// rootDistance.put(currentTag,distance);
-			// try {
-			// if (pst!=null){
-			// requete=Format.executeBlock(st, requete);
-			//
-			// pst.executeBatch();
-			// pst=null;
-			// }
-
 			this.allCols.add(this.currentTag);
-
 			this.requete.append("alter table " + this.tempTableA + " add i" + this.allCols.indexOf(this.currentTag) + " " + this.numBdType + ";");
-//			this.requete.append("alter table " + this.tempTableA + " add i_" + this.currentTag + " " + this.numBdType + ";");
-			
-			// } catch (SQLException e) {
-			// 666 LoggerHelper.errorGenTextAsComment(getClass(), "index()", LOGGER, ex);
-			// e.printStackTrace();
-			// }
+
 
 		} else {
 			// ajouter 1 a son index si la colonne existe dejà
-
 			this.col.put(this.currentTag, (Integer) (o) + 1);
-
-			// if (rootDistance.get(currentTag)!=distance)
-			// {
-			// throw new
-			// SAXParseException("Fichier XML non pris en charge : rubrique placée diferemment dans l'arbre xml  : "+currentTag,"","",0,0);
-			// }
-
 		}
 
 		this.distance++;
@@ -378,22 +271,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 			}
 		}
 
-		// si on retrouve la colonne parmi les colonnes de la ligne en court, on
-		// procède à son insertion
-
-		// System.out.println("***");
-		// System.out.println(currentTag);
-		// System.out.println(father);
-		// System.out.println(leafStatus);
-
-		// if (treeStackFatherLag.indexOf(father)>=0)
-		// {
-		// System.out.println(leafStatus);
-		// System.out.println("***");
-		// }
-
-		// if (lineCols.indexOf(currentTag)>=0)
-
 		/*
 		 * quand le pere de la rubrique fermée est retrouvée et que ce n'est pas
 		 * une feuille qui vient d'etre fermée on va réaliser l'insertion de la
@@ -401,83 +278,34 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 		 */
 
 		if (this.treeStackFatherLag.indexOf(this.father) >= 0 && this.leafStatus == false
-		// modif : updated
-		// && updated==false
 		) {
 
-			// System.out.println(currentTag+" -- Insertion");
-			// try {
-			// nbInsert++;
-
-			// if (pst==null)
-			// {
-			// pst=connexion.prepareStatement(Format.prepareStatementBuilder(colData,allCols));
-			// }
-			// Format.prepareStatementFiller(pst, fileName, colData, allCols,
-			// lineCols, lineIds, lineValues);
-			//
 			insertQueryBuilder(this.requete,this.tempTableA, this.fileName, this.lineCols, this.lineIds, this.lineValues);
 
-			// statement.addBatch(Format.insertQueryBuilder(tableName, nbInsert,
-			// lineCols, lineIds, lineValues));
-
 			if (this.requete.length() > FormatSQL.TAILLE_MAXIMAL_BLOC_SQL) {
-				//StaticLoggerDispatcher.info("Insertion :" + requete.length() + " caractères ", logger);
 
 				try {
 					UtilitaireDao.get("arc").executeImmediate(this.connexion, this.requete);
 				} catch (SQLException ex) {
 					LoggerHelper.errorGenTextAsComment(getClass(), "startElement()", LOGGER, ex);
-					//e.printStackTrace();
 					 throw new SAXParseException("Fichier XML : erreur de requete insertion  : "+ex.getMessage() , "", "", 0, 0);
 				}
 				this.requete.setLength(0);
 				this.start=0;
 			}
 
-			// if (nbInsert%2==0){
-			// // System.out.println("commit");
-			// // statement.executeBatch();
-			// Statement st = connexion.createStatement();
-			// System.out.println(requete);
-			// requete=Format.executeBlock(st, requete);
-			//
-			// //pst.executeBatch();
-			// }
-			//
-			// } catch (SQLException e) {
-			// 666 LoggerHelper.errorGenTextAsComment(getClass(), "index()", LOGGER, ex);
-			// e.printStackTrace();
-			// }
-
-			// on ajoute à la hashmap updateNeeded le pere : si j'amais on
-			// retrouve une feuille directe relative à ce pere, faudra faire un
-			// update des lignes préalablement insérées
-
-			// modif : updated
-			// updateNeeded.put(father, col.get(father));
-
 			// On va alors dépiler ligneCols, lineIds, lineValues jusqu'au père
 			// de la rubrique
 			int fatherIndex = this.lineCols11.lastIndexOf(this.allCols.indexOf(this.father)) + 1;
 
-			//
-			// System.out.println(father);
-			// System.out.println(fatherIndex);
-			// System.out.println(Arrays.toString(lineCols.toArray()));
 
 			Format.removeToIndexInt(this.lineCols11, fatherIndex);
 			Format.removeToIndexInt(this.lineCols, fatherIndex);
 			Format.removeToIndexInt(this.lineIds, fatherIndex);
 			Format.removeToIndex(this.lineValues, fatherIndex);
 
-			// lineCols11=lineCols11.subList(0, fatherIndex);
-			// lineCols=lineCols.subList(0, fatherIndex);
-			// lineIds=lineIds.subList(0, fatherIndex);
-			// lineValues=lineValues.subList(0, fatherIndex);
 		}
 
-		// updated=false;
 
 		this.treeStackFather.add(this.father);
 
@@ -490,17 +318,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 		this.lineIds.add(this.col.get(this.currentTag));
 		this.lineValues.add(null);
 
-		// System.out.println("Parent :"+ligne.size());
-		// System.out.println("colonne : "+ currentTag + " / n°"+
-		// ((Integer)col.get(currentTag)));
-
-		//
-		// ligne.add(name);
-
-		// for (int i = 0; i < atts.getLength(); i++) {
-		// //
-		// System.out.println("attribut "+i+" - "+atts.getName(i)+" : "+atts.getValue(i));
-		// }
 	}
 
 
@@ -523,7 +340,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 	public void insertQueryBuilder(StringBuilder aRequete, String tempTableI, String fileName, List<Integer> lineCols, List<Integer> lineIds,
 			List<String> lineValues) throws SAXParseException {
 
-//		System.out.println("*****");
 
 		HashMap<Integer, String> keep = new HashMap<>();
 
@@ -549,23 +365,14 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 
 		HashMap<Integer, Boolean> doNotinsert = new HashMap<>();
 		for (Map.Entry<Integer, String> entry : this.keepLast.entrySet()) {
-//			System.out.println("****");
-//
-//			System.out.println(entry.getValue());
-//			System.out.println(keep.get(entry.getKey()));
 
 
 			if (entry.getValue().equals(keep.get(entry.getKey())))
 			{
 				doNotinsert.put(entry.getKey(), true);
-				//System.out.println(true);
 
 			}
 		}
-//		System.out.println(tree);
-//		System.out.println(doNotinsert);
-//		System.out.println();
-
 
 		this.keepLast=keep;
 
@@ -589,8 +396,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 
 			// si le bloc est a reinséré (le pere n'est pas retrouvé dans doNotInsert
 			// ou si la colonne est un noeud, on procède à l'insertion
-//			System.out.println("*** "+lineCols.get(i));
-//			System.out.println(doNotinsert.get(tree.get(lineCols.get(i)))==null || tree.containsValue(lineCols.get(i)));
 
 			if (doNotinsert.get(this.tree.get(lineCols.get(i)))==null || this.treeNode.get(lineCols.get(i))!=null)
 			{
@@ -603,14 +408,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 			    req2.append(",'" + lineValues.get(i) + "'");
 			}
 			}
-
-//			else
-//			{
-//			    System.out.println("No insert !");
-//
-//			}
-
-
 		}
 
 		 req.append(")values");
@@ -621,8 +418,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 
 		if (z>-1)
 		{
-		  //  System.out.println("trouvé : "+start+" at "+z);
-
 		    req2.append(",");
 		    aRequete.insert(z+req.length(), req2);
 		}
@@ -632,7 +427,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
 		    aRequete.append(req);
 		    aRequete.append(req2);
 		}
-		//System.out.println(start);
 
 		// en production, on peut vouloir limiter la taille du fichier et le faire passer en KO
 		if (sizeLimit>0)
@@ -654,7 +448,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
             StringBuilder req= new StringBuilder();
 
             int[][] arr = Format.getTreeArrayByDistance(this.tree, this.colDist);
-//          StringBuilder reqCreate = new StringBuilder("WITH noview as (select false as n) \n");
             StringBuilder reqCreate = new StringBuilder(" \n");
 
             StringBuilder reqInsert = new StringBuilder();
@@ -671,33 +464,12 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
             for (int i = 0; i < arr.length; i++) {
 
                 // pour chaque noeud
-//              select distinct i2 m2 ,i0
-//              ,case when i3 is null then max(i3) over (partition by i2) else i3 end as i3
-//              ,case when i3 is null then max(v3) over (partition by i2) else v3 end as v3
-//              ,case when i4 is null then max(i4) over (partition by i2) else i4 end as i4
-//              ,case when i4 is null then max(v4) over (partition by i2) else v4 end as v4
-//              ,case when i5 is null then max(i5) over (partition by i2) else i5 end as i5
-//              ,case when i5 is null then max(v5) over (partition by i2) else v5 end as v5
-//               FROM arc.I where i2 is not null
 
                 if (arr[i][2] == 1) {
 
                     String leaf = Format.getLeafs(arr[i][1], arr, this.colData, this.allCols);
 
                     // créer les vues
-
-                    // Version avec fonctions d'aggregation
-//                    String leafMaxWhenNull = Format.getLeafsMaxWhenNull2(arr[i][1], arr, this.colData, this.allCols);
-//                  reqCreate.append("CREATE TEMPORARY TABLE t_" + allCols.get(arr[i][1]) + " as (select distinct i_" + allCols.get(arr[i][1]) + " m_" + allCols.get(arr[i][1]) + " ");
-//                  if (arr[i][0] >= 0) {
-//                      reqCreate.append(", i_" + allCols.get(arr[i][0]) + " ");
-//                  }
-//                  reqCreate.append(leafMaxWhenNull);
-//                  reqCreate.append(" FROM {table_source} where i_" + allCols.get(arr[i][1]) + " is not null); \n");
-
-
-
-                    // Version sans fonctions d'aggregation
                     String leafMax = Format.getLeafsMax(arr[i][1], arr, this.colData, this.allCols);
                     reqCreate.append("CREATE TEMPORARY TABLE t_" + this.allCols.get(arr[i][1]) + " as (select i_" + this.allCols.get(arr[i][1]) + " as m_" + this.allCols.get(arr[i][1]) + " ");
                     if (arr[i][0] >= 0) {
@@ -755,16 +527,6 @@ public class XMLHandlerCharger4 extends org.xml.sax.helpers.DefaultHandler {
             req.append (reqSelect);
             req.append (reqFrom);
 
-
-            // la compression fait perdre trop de temp
-            // Mais apres, c'est sur que la table de pilotage devient un peu trop "grosse"
-
-//            try {
-//              this.jointure=StringCompress.compress(req.toString());
-//            } catch (IOException e) {
-//              666 LoggerHelper.errorGenTextAsComment(getClass(), "index()", LOGGER, ex);
-//              e.printStackTrace();
-//            }
             this.jointure=req.toString().replace("'", "''");
 
     }
