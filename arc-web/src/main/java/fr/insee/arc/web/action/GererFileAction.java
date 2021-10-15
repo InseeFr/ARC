e928342691385cd4ecba741f8aec394ffda52665
Manuel Soulier <manuel.soulier@insee.fr>
2021-10-15 18:13:14 +0200

    debug screen code quality

--- arc-web/src/main/java/fr/insee/arc/web/action/GererFileAction.java
+++ arc-web/src/main/java/fr/insee/arc/web/action/GererFileAction.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,6 +21,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
 
+import fr.insee.arc.utils.dao.PreparedStatementBuilder;
 import fr.insee.arc.web.model.FileSystemManagementModel;
 import fr.insee.arc.web.util.VObject;
 import fr.insee.arc.web.util.VObjectService;
@@ -36,7 +40,12 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 
 	private static final String RESULT_SUCCESS = "jsp/gererFile.jsp";
 
-	public static String REPERTOIRE_EFFACABLE="TO_DELETE";
+	// magic word that must b e found in file or directory name to proceed for deletion
+	private static final String DELETABLE="DELETE";
+	
+	// view column name containing the filename
+	private static final String VC_FILENAME ="filename";
+
 
 	private VObject viewDirIn;
 
@@ -94,7 +103,7 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 	public String seeDirIn(Model model) {
 		Map<String,ArrayList<String>> m= viewDirIn.mapContentSelected();
 		if (!m.isEmpty() && m.get(IS_DIRECTORY).get(0).equals("true"))  {
-			this.dirIn= Paths.get(this.dirIn, m.get("filename").get(0)).toString() + File.separator;
+			this.dirIn= Paths.get(this.dirIn, m.get(VC_FILENAME).get(0)).toString() + File.separator;
 			model.addAttribute(DIR_IN, this.dirIn);
 		}
 		return generateDisplay(model, RESULT_SUCCESS);
@@ -102,62 +111,48 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 
 	@RequestMapping("/sortDirIn")
 	public String sortDirIn(Model model) {
-		return sortVobject(model, RESULT_SUCCESS, viewDirIn);
+		return sortVobject(model, RESULT_SUCCESS, this.viewDirIn);
 	}
 
 	@RequestMapping("/transferDirIn")
 	public String transferDirIn(Model model) {
-		transfer(viewDirIn, this.dirIn, this.dirOut);
+		transfer(this.viewDirIn, this.dirIn, this.dirOut);
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
 	@RequestMapping("/copyDirIn")
 	public String copyDirIn(Model model) {
-		copy(viewDirIn, this.dirIn, this.dirOut);
+		copy(this.viewDirIn, this.dirIn, this.dirOut);
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
-	@RequestMapping("/renameDirIn")
-	public String renameDirIn(Model model) {
-		Map<String,ArrayList<String>> m=viewDirIn.mapContentSelected();
-		Map<String,ArrayList<String>> n=viewDirIn.mapContentSelected();
-
-		if (!m.isEmpty())
-		{
-			for (int i=0; i<m.get("filename").size();i++)
-			{
-				File fileIn = new File(this.dirIn + m.get("filename").get(i));
-				File fileOut = new File(this.dirIn + n.get("filename").get(i));
-				fileIn.renameTo(fileOut);
-			}
-		}
+	@RequestMapping("/updateDirIn")
+	public String updateDirIn(Model model) {
+		rename(this.viewDirIn,this.dirIn);
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
-
 
 	@RequestMapping("/addDirIn")
 	public String addDirIn(Model model) {
-		HashMap<String,ArrayList<String>> m=viewDirIn.mapInputFields();
-		if (!m.isEmpty())
-		{
-			if (m.get("filename").get(0)!=null && !m.get("filename").get(0).trim().equals(""))
-			{
-				FileUtils.createDirectory(this.dirIn+m.get("filename").get(0).trim());
-			}
-		}
+		createDirectory(this.viewDirIn,this.dirIn);
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
 	@RequestMapping("/deleteDirIn")
 	public String delDirIn(Model model) {
-		if (delete(viewDirIn, this.dirIn, this.dirOut))
+		if (delete(this.viewDirIn, this.dirIn))
 		{
-			this.dirIn=null;
+			this.dirIn=properties.getBatchParametersDirectory();
 			model.addAttribute(DIR_IN, this.dirIn);
 		}
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
+	@RequestMapping("/downloadDirIn")
+	public String downloadDirIn(Model model, HttpServletResponse response) {
+		download(response, this.viewDirIn, this.dirIn);
+        return "none";
+	}
 
 
 	// private SessionMap session;
@@ -178,7 +173,7 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 
 		if (!m.isEmpty()) {
 			if(m.get(IS_DIRECTORY).get(0).equals("true")) {
-				this.dirOut = Paths.get(this.dirOut, m.get("filename").get(0)).toString() + File.separator;
+				this.dirOut = Paths.get(this.dirOut, m.get(VC_FILENAME).get(0)).toString() + File.separator;
 				model.addAttribute(DIR_OUT, this.dirOut);
 			}
 		}
@@ -188,72 +183,64 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 
 	@RequestMapping("/sortDirOut")
 	public String sortDirOut(Model model) {
-		this.vObjectService.sort(viewDirOut);
-		return generateDisplay(model, RESULT_SUCCESS);
+		return sortVobject(model, RESULT_SUCCESS, this.viewDirOut);
 	}
 
 	@RequestMapping("/transferDirOut")
 	public String transferDirOut(Model model) {
-		transfer(viewDirOut, this.dirOut, this.dirIn);
+		transfer(this.viewDirOut, this.dirOut, this.dirIn);
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
 
 	@RequestMapping("/copyDirOut")
 	public String copyDirOut(Model model) {
-		copy(viewDirOut, this.dirOut, this.dirIn);
+		copy(this.viewDirOut, this.dirOut, this.dirIn);
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
-	@RequestMapping("/renameDirOut")
-	public String renameDirOut(Model model) {
-		Map<String,ArrayList<String>> m=viewDirOut.mapContentSelected();
-		Map<String,ArrayList<String>> n=viewDirOut.mapContentSelected();
-
-		if (!m.isEmpty())
-		{
-			for (int i=0; i<m.get("filename").size();i++)
-			{
-				File fileIn = new File(this.dirOut + m.get("filename").get(i));
-				File fileOut = new File(this.dirOut + n.get("filename").get(i));
-				fileIn.renameTo(fileOut);
-			}
-		}
+	@RequestMapping("/updateDirOut")
+	public String updateDirOut(Model model) {
+		rename(this.viewDirOut,this.dirOut);
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
 
 	@RequestMapping("/addDirOut")
 	public String addDirOut(Model model) {
-		HashMap<String,ArrayList<String>> m=viewDirOut.mapInputFields();
-		if (!m.isEmpty())
-		{
-			if (m.get("filename").get(0)!=null && !m.get("filename").get(0).trim().equals(""))
-			{
-				FileUtils.createDirectory(this.dirOut+m.get("filename").get(0).trim());
-			}
-		}
-
-
+		createDirectory(this.viewDirOut,this.dirOut);
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
 	@RequestMapping("/deleteDirOut")
 	public String delDirOut(Model model) {
-		if (delete(viewDirOut, this.dirOut, this.dirIn))
+		if (delete(this.viewDirOut, this.dirOut))
 		{
-			this.dirOut=null;
+			this.dirOut=properties.getBatchParametersDirectory();
 			model.addAttribute(DIR_OUT, this.dirOut);
 		}
 		return generateDisplay(model, RESULT_SUCCESS);
 	}
 
 	
+	@RequestMapping("/downloadDirOut")
+	public String downloadDirOut(Model model, HttpServletResponse response) {
+		download(response, this.viewDirOut, this.dirOut);
+        return "none";
+	}
+	
+	
+	/**
+	 * Transfer the files selected in the view viewSource from the source directory to the target directory
+	 * @param viewSource
+	 * @param dirSource
+	 * @param dirTarget
+	 */
 	private void transfer(VObject viewSource, String dirSource, String dirTarget)
 	{
 		Map<String, ArrayList<String>> m = viewSource.mapContentSelected();
 		if (!m.isEmpty()) {
-			for (String f : m.get("filename")) {
+			for (String f : m.get(VC_FILENAME)) {
 				File fileSource = Paths.get(dirSource, f).toFile();
 				File fileTarget = Paths.get(dirTarget, f).toFile();
 				if (!fileSource.renameTo(fileTarget))
@@ -280,48 +267,115 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 		}
 	}
 	
-	private boolean delete(VObject viewSource, String dirSource, String dirTarget) {
+	/**
+	 * Delete the file or folder selected by the users in the view
+	 * the name of files or folders to be deleted must contain a magic word to proceed
+	 * @param viewSource
+	 * @param dirSource
+	 * @return
+	 */
+	private boolean delete(VObject viewSource, String dirSource) {
 
+		
+		System.out.println("del on "+dirSource);
+		
+		// safeguard : only file or directory containing this magic word will be able to be deleted	
 		Map<String, ArrayList<String>> m = viewSource.mapContentSelected();
 		if (!m.isEmpty()) {
-			for (String f : m.get("filename")) {
+			for (String f : m.get(VC_FILENAME)) {
 				File fileSource = new File(dirSource + f);
 				if (fileSource.isFile()) {
-					if (!fileSource.delete())
+					if (!fileSource.getName().contains(DELETABLE))
 					{
-						String errorMessage = "An error occured while deleting the file "+fileSource;
-						loggerDispatcher.error(errorMessage, LOGGER);
+						String errorMessage = "file must contain the word DELETE to be deleted : "+fileSource;
 						viewSource.setMessage(errorMessage);
+					}
+					else
+					{
+						if (!fileSource.delete())
+						{
+							String errorMessage = "An error occured while deleting the file "+fileSource;
+							viewSource.setMessage(errorMessage);
+						}
 					}
 				}
 			}
 			return false;
 		}
 		
-			File dirSourceFile = new File(dirSource);
-			if (dirSourceFile.listFiles().length == 0) {
-				FileUtils.delete(dirSource);
-				return true;
-			} else {
-				for (File f : dirSourceFile.listFiles()) {
-					if (f.isFile()) {
-						if (!f.delete())
-						{
-							String errorMessage = "An error occured while deleting the file "+f;
-							loggerDispatcher.error(errorMessage, LOGGER);
-							viewSource.setMessage(errorMessage);
-						}
+		if (!dirSource.contains(DELETABLE))
+		{
+			return false;
+		}
+		
+		File dirSourceFile = new File(dirSource);
+		if (dirSourceFile.listFiles().length == 0) {
+			FileUtils.delete(dirSource);
+			return true;
+		} else {
+			for (File f : dirSourceFile.listFiles()) {
+				if (f.isFile()) {
+					if (!f.delete()) {
+						String errorMessage = "An error occured while deleting the file " + f;
+						viewSource.setMessage(errorMessage);
 					}
 				}
-				return false;
 			}
+			return false;
+		}
+	}
+	
+
+	/**
+	 * Rename a file or directory according to the changed made by user in view
+	 * @param model
+	 * @return
+	 */
+	private void rename(VObject viewSource, String dirSource) {
+		
+		Map<String,ArrayList<String>> m0=viewSource.mapContentBeforeUpdate();
+		Map<String,ArrayList<String>> m1=viewSource.mapContentAfterUpdate();
+		
+		for (int i=0; i<m0.get(VC_FILENAME).size();i++)
+		{
+			File fileIn = new File(dirSource+ m0.get(VC_FILENAME).get(i));
+			File fileOut = new File(dirSource + m1.get(VC_FILENAME).get(i));
+			fileIn.renameTo(fileOut);
+		}
+	}
+	
+	
+	public void download(HttpServletResponse response, VObject viewSource, String dirSource) {
+		HashMap<String, ArrayList<String>> selection = viewSource.mapContentSelected();
+    	if (!selection.isEmpty())
+    	{
+
+    		PreparedStatementBuilder requete=new PreparedStatementBuilder();
+    		boolean first=true;
+    		for (String s:selection.get(VC_FILENAME))
+    		{
+    			if (first)
+    			{
+    				first=false;
+    			}
+    			else
+    			{
+    				requete.append("\n UNION ALL ");
+    			}
+    			requete.append("SELECT "+requete.quoteText(s)+" as nom_fichier ");
+    		}
+    		
+    		ArrayList<String> r=new ArrayList<String>(Arrays.asList("/"));
+
+        this.vObjectService.downloadEnveloppe(viewSource, response, requete, dirSource, r);
+	}
 	}
 	
 	private void copy(VObject viewSource, String dirSource, String dirTarget) {
 		Map<String,ArrayList<String>> m = viewSource.mapContentSelected();
 		if (!m.isEmpty())
 		{
-			for (String f:m.get("filename"))
+			for (String f:m.get(VC_FILENAME))
 			{
 				File fileSource = new File(dirSource + f);
 				File fileTarget = new File(dirTarget + f);
@@ -353,6 +407,29 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 		}
 	}
 
+	/**
+	 * Create a new directory according to the name provided by the user in the source view
+	 * @param viewSource
+	 * @param dirSource
+	 */
+	private void createDirectory(VObject viewSource, String dirSource)
+	{
+		HashMap<String,ArrayList<String>> m=viewSource.mapInputFields();
+		if (!m.isEmpty())
+		{
+			if (m.get(VC_FILENAME).get(0)!=null && !m.get(VC_FILENAME).get(0).trim().equals(""))
+			{
+				FileUtils.createDirectory(dirSource+m.get(VC_FILENAME).get(0).trim());
+			}
+		}
+	}
+	
+	/**
+	 * Return the files and directories from a parent directory and according to the view filters
+	 * @param dirUri
+	 * @param dirVobject
+	 * @return
+	 */
 	private ArrayList<ArrayList<String>> getDirFiles(String dirUri, VObject dirVobject) {
 		File dirFile = Paths.get(dirUri).toFile();
 		ArrayList<ArrayList<String>> listeFichier = new ArrayList<>();
@@ -366,7 +443,7 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 	
 	private void initializeArrayForDirVObject(ArrayList<ArrayList<String>> result)
 	{
-		VObjectService.addRowToVObjectList(result,"filename",IS_DIRECTORY);
+		VObjectService.addRowToVObjectList(result,VC_FILENAME,IS_DIRECTORY);
 		VObjectService.addRowToVObjectList(result,"text","text");
 	}
 
@@ -389,13 +466,13 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 			filter=new  HashMap<>();
 		}
 		
-		filter.putIfAbsent("filename", new ArrayList<>());
+		filter.putIfAbsent(VC_FILENAME, new ArrayList<>());
 		filter.putIfAbsent(IS_DIRECTORY, new ArrayList<>());
 
-		if (filter.get("filename").isEmpty()) {
-			filter.get("filename").add("");
-		} else if (filter.get("filename").get(0) == null) {
-			filter.get("filename").set(0, "");
+		if (filter.get(VC_FILENAME).isEmpty()) {
+			filter.get(VC_FILENAME).add("");
+		} else if (filter.get(VC_FILENAME).get(0) == null) {
+			filter.get(VC_FILENAME).set(0, "");
 		}
 
 		if (filter.get(IS_DIRECTORY).isEmpty()) {
@@ -407,7 +484,7 @@ public class GererFileAction extends ArcAction<FileSystemManagementModel> {
 
 		for (File f: dir.listFiles()) {
 			boolean toInsert=true;
-			if (!filter.get("filename").get(0).equals("") && !f.getName().contains(filter.get("filename").get(0))) {
+			if (!filter.get(VC_FILENAME).get(0).equals("") && !f.getName().contains(filter.get(VC_FILENAME).get(0))) {
 				toInsert=false;
 			}
 
--- arc-web/src/main/webapp/WEB-INF/jsp/gererFile.jsp
+++ arc-web/src/main/webapp/WEB-INF/jsp/gererFile.jsp
@@ -12,7 +12,13 @@
 </head>
 
 <body class="bg-body">
-<form action="gererFile" spellcheck="false"method="post">
+<form
+	spellcheck="false"
+	id="gererFile"
+	action="selectFile.action"
+	method="post"
+	accept-charset="UTF-8"
+>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-5 border-right">
@@ -24,6 +30,7 @@
 				<c:param name="btnSee" value="true" />
 				<c:param name="btnAdd" value="true" />
 				<c:param name="btnDelete" value="true" />
+				<c:param name="btnUpdate" value="true" />
 				<c:param name="ligneFilter" value ="true" />
 				<c:param name="multiSelection" value ="true" />
 				<c:param name="checkbox" value="true" />
@@ -34,6 +41,7 @@
 					<input id="viewDirIn.transfer" type="submit" doAction="transferDirIn" scope="viewDirIn;viewDirOut;" value="Transférer"></input>
 					<input id="viewDirIn.update" type="submit" doAction="renameDirIn" scope="viewDirIn;viewDirOut;" value="Mettre à jour" style="display:none;"></input>
 					<input id="viewDirIn.copy" type="submit" doAction="copyDirIn" scope="viewDirIn;viewDirOut;" value="Copier"></input>
+					<input id="viewDirIn.downloadDirIn" type="submit" doAction="downloadDirIn" ajax="false" value="Download"></input>
 				</c:param>
 			</c:import>
 		</div>
@@ -50,14 +58,17 @@
 				<c:param name="btnSelect" value="true" />
 				<c:param name="btnAdd" value="true" />
 				<c:param name="btnDelete" value="true" />
+				<c:param name="btnUpdate" value="true" />
 				<c:param name="checkbox" value="true" />
 				<c:param name="checkboxVisible" value="true" />
+				<c:param name="multiSelection" value ="true" />
 				<c:param name="extraScopeSee" value ="viewDirIn;viewDirOut;" />
 				<c:param name="otherButton">
 					<input type="text" m="" action='select' name="dirOut" value="${dirOut}" />
 					<input id="viewDirOut.transfer" type="submit" doAction="transferDirOut" scope="viewDirIn;viewDirOut;" value="Transférer"></input>
 					<input id="viewDirOut.update" type="submit" doAction="renameDirOut" scope="viewDirIn;viewDirOut;" value="Mettre à jour" style="display:none;"></input>
 					<input id="viewDirOut.copy" type="submit" doAction="copyDirOut" scope="viewDirIn;viewDirOut;" value="Copier"></input>
+					<input id="viewDirIn.downloadDirOut" type="submit" doAction="downloadDirOut" ajax="false" value="Download"></input>
 				</c:param>
 			</c:import>
 		</div>
