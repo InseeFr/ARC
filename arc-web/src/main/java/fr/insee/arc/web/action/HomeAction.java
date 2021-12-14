327d429227a3ab29f5ddbf31f6f93af900ff90c3
Manuel Soulier <manuel.soulier@insee.fr>
2021-12-14 14:42:53 +0100

    home.jsp action (made for security purpose)

---
+++ arc-web/src/main/java/fr/insee/arc/web/action/HomeAction.java
@@ -0,0 +1,40 @@
+package fr.insee.arc.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONObject;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.WebApplicationContext;
+
+import fr.insee.arc.web.model.NoModel;
+
+@Controller
+@Scope(scopeName = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class HomeAction extends ArcAction<NoModel> {
+
+    public static final String ACTION_NAME = "home";
+    private static final String RESULT_SUCCESS = "jsp/home.jsp";
+    
+    @RequestMapping("/")
+    public String index(Model model, HttpServletRequest request) {
+    	return generateDisplay(model, RESULT_SUCCESS);
+    }
+
+
+
+    @Override
+    public void putAllVObjects(NoModel arcModel) {
+    	// no vObject in this controller
+    }
+
+    @Override
+    public String getActionName() {
+    	return ACTION_NAME;
+    }
+    
+}
