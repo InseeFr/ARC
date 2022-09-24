8700429fa005355dbe91d086c4925d3543ea53aa
Manuel Soulier <manuel.soulier@insee.fr>
2022-09-24 21:58:49 +0200

    dummy test class

--- arc-batch/src/main/java/fr/insee/arc/batch/AppTestTemp.java
+++ arc-batch/src/main/java/fr/insee/arc/batch/AppTestTemp.java
@@ -1,10 +1,12 @@
 package fr.insee.arc.batch;
 
+import java.sql.SQLException;
+
 public class AppTestTemp {
 	 
 	
-	public void execute(String[] args) {
-
+	public void execute(String[] args) throws SQLException {
+		// dummy test class
 	}
 	
 }
