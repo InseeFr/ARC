32cb7faaa856bd0de3e72b5d6e8d96c4ecaca247
Manuel Soulier <manuel.soulier@insee.fr>
2022-10-10 17:26:55 +0200

    fix in ModeRequeteImpl test class calling methods

--- arc-utils/src/test/java/fr/insee/arc/utils/dao/ModeRequeteImplTest.java
+++ arc-utils/src/test/java/fr/insee/arc/utils/dao/ModeRequeteImplTest.java
@@ -1,35 +1,26 @@
 package fr.insee.arc.utils.dao;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class ModeRequeteImplTest {
 
-	 
-	
-	public static final String PARALLEL_WORK_MEM = "24MB";
 
-	public static final String SORT_WORK_MEM="128MB";
-   
-   public static final int TIME_OUT_SQL_EN_HEURE = 100;
-	
-   public static final String  COLLAPSE_JOIN_LIMIT = "10000";
-   
-   
-   
-   @Test
-   public static void arcModeRequeteIHMTest() {
-   	String query="set enable_hashjoin=on;set enable_mergejoin=off;set enable_hashagg=on;set enable_seqscan=off;set enable_material=off;\n";
-    assertEquals(query,ModeRequete.untokenize(ModeRequeteImpl.arcModeRequeteIHM()));
-   }
-   
-	
 	@Test
-	public static void arcModeRequeteEngineTest() {
+	public void arcModeRequeteIHMTest() {
+		String query = "set enable_hashjoin=on;set enable_mergejoin=off;set enable_hashagg=on;set enable_seqscan=off;set enable_material=off;\n";
+		assertEquals(query, ModeRequete.untokenize(ModeRequeteImpl.arcModeRequeteIHM()));
+	}
+
+	@Test
+	public void arcModeRequeteEngineTest() {
 		
-		String defaultSchema="arc_bas1";
-		
+		String PARALLEL_WORK_MEM = "24MB";
+
+		int TIME_OUT_SQL_EN_HEURE = 100;
+
+		String defaultSchema = "arc_bas1";
+
 		StringBuilder query = new StringBuilder();
 
 		query.append("set enable_nestloop=on;").append("set enable_mergejoin=off;").append("set enable_hashjoin=on;")
@@ -42,8 +33,8 @@ public class ModeRequeteImplTest {
 				.append("set search_path=" + defaultSchema.toLowerCase() + ", public;")
 				.append(ModeRequete.EXTRA_FLOAT_DIGIT.expr()).append("COMMIT;");
 
-		assertEquals(query.toString(),ModeRequeteImpl.arcModeRequeteEngine(defaultSchema));
- 
+		assertEquals(query.toString(), ModeRequeteImpl.arcModeRequeteEngine(defaultSchema));
+
 	}
 
 }
