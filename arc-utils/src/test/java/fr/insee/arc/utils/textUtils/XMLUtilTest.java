278cc7bcdf78da6b850b4093846d41fc5b28c8dd
Manuel Soulier <manuel.soulier@insee.fr>
2022-12-04 16:50:15 +0100

    XMLUtil test coverage

--- arc-utils/src/test/java/fr/insee/arc/utils/textUtils/XMLUtilTest.java
+++ arc-utils/src/test/java/fr/insee/arc/utils/textUtils/XMLUtilTest.java
@@ -7,13 +7,28 @@ import org.junit.Test;
 public class XMLUtilTest extends XMLUtil {
 	
 	@Test
+	/*
+	 * commn usecase; return value between tags
+	 */
 	public void parseXML1() {
 		assertEquals("2<9 is true",parseXML("<my_tag>2<9 is true</my_tag>","my_tag"));
 	}
 
 	@Test
+	/*
+	 * refer only to the first tag; closing tag is optional
+	 */
 	public void parseXML2() {
 		assertEquals("2<9 is true",parseXML("<my_tag>2<9 is true","my_tag"));
+	}
+	
+	
+	@Test
+	/**
+	 * if input null, return null
+	 */
+	public void parseXMLnull() {
+		assertEquals(null,parseXML(null,"my_tag"));
 	}
 	
 }
