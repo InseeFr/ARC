12aa4329781108029e197343eb0bc6a8157517bc
Manuel Soulier <manuel.soulier@insee.fr>
2023-03-27 12:24:08 +0200

    FastList utility : same as list but built with bi directionnal map for performance on indexOf

---
+++ arc-utils/src/test/java/fr/insee/arc/utils/textUtils/FastListTest.java
@@ -0,0 +1,63 @@
+package fr.insee.arc.utils.textUtils;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FastListTest {
+
+	@Test
+	public void testFastListAddElementOneByOne() {
+
+	FastList<String> myFastList;
+	myFastList = new FastList<>();
+	
+	myFastList.add("s");
+	myFastList.add("e");
+	myFastList.add("u");
+	
+	List<String> myArrayList = new ArrayList<>(Arrays.asList("s","e","u"));
+
+	// list equality
+	assertEquals(myArrayList, myFastList.asList());
+	
+	// list index of
+	assertEquals(myArrayList.indexOf("e"),myFastList.indexOf("e"));
+	assertEquals(myArrayList.indexOf("unknown"),myFastList.indexOf("unknown"));
+	
+	}
+	
+	
+	@Test
+	public void testFastListAddElementByList() {
+	FastList<String> myFastList = new FastList<>(Arrays.asList("s","e","u"));
+	List<String> myArrayList = new ArrayList<>(Arrays.asList("s","e","u"));
+
+	// list equality
+	assertEquals(myArrayList, myFastList.asList());
+	
+	// list index of
+	assertEquals(myArrayList.indexOf("e"),myFastList.indexOf("e"));
+	assertEquals(myArrayList.indexOf("unknown"),myFastList.indexOf("unknown"));
+
+	}
+	
+	@Test
+	public void testSubList() {
+	FastList<String> myFastList = new FastList<>(Arrays.asList("s","e","u"));
+	List<String> myArrayList = new ArrayList<>(Arrays.asList("s","e","u"));
+	
+	// sub-listing
+	List<String> subListOfMyArrayList=myArrayList.subList(1, 2);
+	List<String> subListOfMyFastList=myFastList.subList(1, 2);
+	
+	assertEquals(subListOfMyArrayList,subListOfMyFastList);
+	
+	}
+	
+
+}
