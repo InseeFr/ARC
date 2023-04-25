62330379295dae968e3c9e441140efa46b88a852
Manuel Soulier <manuel.soulier@insee.fr>
2023-04-25 18:14:38 +0200

    Utility class test

---
+++ arc-utils/src/test/java/fr/insee/arc/utils/utils/PrivateConstructorTest.java
@@ -0,0 +1,25 @@
+package fr.insee.arc.utils.utils;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+public class PrivateConstructorTest {
+
+	@Test
+	public static void testConstructorIsPrivate(final Class<?> clazz) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+	    Constructor<?> constructor = clazz.getDeclaredConstructor();
+	    assertTrue(Modifier.isPrivate(constructor.getModifiers())); //this tests that the constructor is private
+	    constructor.setAccessible(true);
+	    assertThrows(InvocationTargetException.class, () -> {
+	        constructor.newInstance();
+	    }); //this add the full coverage on private constructor
+	}
+	
+	
+}
