17d936340a6f5035d6c498c2faaed428299db2fe
Manuel Soulier <manuel.soulier@insee.fr>
2023-07-06 15:59:47 +0200

    fix annotation bug

--- arc-web/src/main/java/fr/insee/arc/web/gui/pilotage/controller/ControllerRapportBAS.java
+++ arc-web/src/main/java/fr/insee/arc/web/gui/pilotage/controller/ControllerRapportBAS.java
@@ -1,14 +1,14 @@
 package fr.insee.arc.web.gui.pilotage.controller;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.insee.arc.web.gui.pilotage.service.ServiceViewRapportBAS;
 
-@Service
+@Controller
 public class ControllerRapportBAS extends ServiceViewRapportBAS {
- 
+
 	@RequestMapping("/selectRapportBAS")
 	public String selectRapportBASAction(Model model) {
 		return selectRapportBAS(model);
@@ -18,5 +18,5 @@ public class ControllerRapportBAS extends ServiceViewRapportBAS {
 	public String sortRapportBASAction(Model model) {
 		return sortRapportBAS(model);
 	}
-	
+
 }
