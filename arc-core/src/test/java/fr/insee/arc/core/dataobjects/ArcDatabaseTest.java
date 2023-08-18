5583a565e27b6443a197c7418695f92d01d2fea2
Manuel Soulier <manuel.soulier@insee.fr>
2023-08-18 16:52:28 +0200

    number of executor nod test fix

--- arc-core/src/test/java/fr/insee/arc/core/dataobjects/ArcDatabaseTest.java
+++ arc-core/src/test/java/fr/insee/arc/core/dataobjects/ArcDatabaseTest.java
@@ -11,33 +11,19 @@ public class ArcDatabaseTest extends InitializeQueryTest {
 	@Test
 	public void numberOfExecutorNods_testMultiWithExecutor() {
 		// settings 4 database
-		// 1 is meta data
-		// 2 is coordinator
-		// 3+ are executor
+		// 1 is coordinator
+		// 2+ are executor
 		u.getProperties().setDatabaseUsername("arc|||arc|||arc|||arc");
 		
-		// must return 2 executor nods
-		assertEquals(2,ArcDatabase.numberOfExecutorNods());
-	}
-	
-	@Test
-	public void numberOfExecutorNods_testMultiJustCoodinator() {
-		// settings 4 database
-		// 1 is meta data
-		// 2 is coordinator
-		// 3+ are executor
-		u.getProperties().setDatabaseUsername("arc|||arc");
-		
-		// must return 0 executor nods
-		assertEquals(0,ArcDatabase.numberOfExecutorNods());
+		// must return 3 executor nods
+		assertEquals(3,ArcDatabase.numberOfExecutorNods());
 	}
 	
 	@Test
 	public void numberOfExecutorNods_testMultiJustExecutor() {
-		// settings 4 database
-		// 1 is meta data
-		// 2 is coordinator
-		// 3+ are executor
+		// settings 1 database
+		// 1 is coordinator
+		// 2+ are executor
 		u.getProperties().setDatabaseUsername("arc");
 		
 		// must return 0 executor nods
