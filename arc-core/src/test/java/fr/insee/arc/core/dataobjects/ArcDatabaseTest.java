3dd37f8681d09d37e30e21a64c0643af4cea5047
Manuel Soulier <manuel.soulier@insee.fr>
2023-10-08 07:49:51 +0200

    fix: ArcDatabase tests

--- arc-core/src/test/java/fr/insee/arc/core/dataobjects/ArcDatabaseTest.java
+++ arc-core/src/test/java/fr/insee/arc/core/dataobjects/ArcDatabaseTest.java
@@ -10,22 +10,27 @@ public class ArcDatabaseTest extends InitializeQueryTest {
 
 	@Test
 	public void numberOfExecutorNods_testMultiWithExecutor() {
-		// settings 4 database
-		// 1 is coordinator
-		// 2+ are executor
-		u.getProperties().setDatabaseUsername("arc|||arc|||arc|||arc");
-		
-		// must return 3 executor nods
-		assertEquals(3,ArcDatabase.numberOfExecutorNods());
+		u.getProperties().setDatabaseUrl("{0=>\"db1\"},{1=>\"db2\"}");
+		u.getProperties().setDatabaseUsername("{0=>\"db1\"},{1=>\"db2\"}");
+		u.getProperties().setDatabasePassword("{0=>\"db1\"},{1=>\"db2\"}");
+		u.getProperties().setDatabaseDriverClassName("{0=>\"db1\"},{1=>\"db2\"}");
+
+		// must return 1 nods
+		assertEquals(1,ArcDatabase.numberOfExecutorNods());
 	}
 	
+	
+	
 	@Test
-	public void numberOfExecutorNods_testMultiJustExecutor() {
+	public void numberOfExecutorNods_testMultiNoExecutor() {
 		// settings 1 database
 		// 1 is coordinator
 		// 2+ are executor
-		u.getProperties().setDatabaseUsername("arc");
-		
+		u.getProperties().setDatabaseUrl("arc");
+		u.getProperties().setDatabaseUsername("usr");
+		u.getProperties().setDatabasePassword("pwd");
+		u.getProperties().setDatabaseDriverClassName("pg");
+				
 		// must return 0 executor nods
 		assertEquals(0,ArcDatabase.numberOfExecutorNods());
 	}
