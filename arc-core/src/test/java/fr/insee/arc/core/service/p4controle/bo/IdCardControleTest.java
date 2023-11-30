6097d5ea2adb1166286ff8eaae244e47b2eda385
Manuel Soulier <manuel.soulier@insee.fr>
2023-11-30 02:02:07 +0100

    fix: IdCardControleTest

---
+++ arc-core/src/test/java/fr/insee/arc/core/service/p4controle/bo/IdCardControleTest.java
@@ -0,0 +1,28 @@
+package fr.insee.arc.core.service.p4controle.bo;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class IdCardControleTest {
+
+	@Test
+	public void getReglesControleByControleTypeCode() {
+		
+		List<RegleControle> reglesControle = new ArrayList<>();
+		reglesControle.add(new RegleControle(ControleTypeCode.ALPHANUM, null, null, null, null, null, null, 0, null, null));
+		reglesControle.add(new RegleControle(ControleTypeCode.ALPHANUM, null, null, null, null, null, null, 1, null, null));
+		reglesControle.add(new RegleControle(ControleTypeCode.CARDINALITE, null, null, null, null, null, null, 2, null, null));
+		reglesControle.add(new RegleControle(ControleTypeCode.ALPHANUM, null, null, null, null, null, null, 3, null, null));
+
+		IdCardControle idCardControle = new IdCardControle(reglesControle);
+		
+		List<RegleControle> reglesControleALPHANUM = idCardControle.getReglesControle(ControleTypeCode.ALPHANUM);
+		assertEquals(3, reglesControleALPHANUM.size());
+
+	}
+
+}
