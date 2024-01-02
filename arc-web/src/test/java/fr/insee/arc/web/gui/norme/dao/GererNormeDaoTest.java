72ee6978e047717a30e4c7046eb4f65c58a12503
Léo Manelphe <leo.manelphe@insee.fr>
2024-01-02 15:12:05 +0100

    feat: test uploadFileMapping

--- arc-web/src/test/java/fr/insee/arc/web/gui/norme/dao/GererNormeDaoTest.java
+++ arc-web/src/test/java/fr/insee/arc/web/gui/norme/dao/GererNormeDaoTest.java
@@ -2,11 +2,20 @@ package fr.insee.arc.web.gui.norme.dao;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import fr.insee.arc.core.dataobjects.ArcPreparedStatementBuilder;
 import fr.insee.arc.core.dataobjects.ColumnEnum;
@@ -24,6 +33,7 @@ import fr.insee.arc.web.gui.all.util.VObjectService;
 import fr.insee.arc.web.gui.norme.model.ViewCalendrier;
 import fr.insee.arc.web.gui.norme.model.ViewChargement;
 import fr.insee.arc.web.gui.norme.model.ViewJeuxDeRegles;
+import fr.insee.arc.web.gui.norme.model.ViewMapping;
 import fr.insee.arc.web.gui.norme.model.ViewNorme;
 
 public class GererNormeDaoTest extends InitializeQueryTest {
@@ -33,9 +43,12 @@ public class GererNormeDaoTest extends InitializeQueryTest {
 	private static GererNormeDao pdao;
 
 	@BeforeClass
-	public static void setup() throws ArcException {
+	public static void setup() throws ArcException, SQLException {
+		
+		buildPropertiesWithoutScalability(null);
+		
 		BddPatcherTest.createDatabase();
-		BddPatcherTest.insertTestDataLight();
+		BddPatcherTest.insertTestDataSiera();
 		vObjectService = new VObjectService();
 		vObjectService.setConnection(c);
 		vObjectService.setSession(new Session());
@@ -43,6 +56,7 @@ public class GererNormeDaoTest extends InitializeQueryTest {
 		dao.setSandboxSchema(BddPatcherTest.testSandbox1);
 		pdao = new GererNormeDao();
 		pdao.initialize(vObjectService, dao);
+
 	}
 
 	@Test
@@ -61,7 +75,7 @@ public class GererNormeDaoTest extends InitializeQueryTest {
 		assertEquals(0, viewColumns.size());
 		
 		// in test data, must return two norms
-		assertEquals(2, viewNorme.getContent().t.size());
+		assertEquals(1, viewNorme.getContent().t.size());
 
 	}
 
@@ -72,7 +86,7 @@ public class GererNormeDaoTest extends InitializeQueryTest {
 		
 		// select the first record of viewNorm and set it as the selected record
 		ArcPreparedStatementBuilder query = new ArcPreparedStatementBuilder();
-		query.build(SQL.SELECT, "*", SQL.FROM, pdao.getDataObjectService().getView(ViewEnum.IHM_NORME), SQL.WHERE, "id_norme='v2016-02'");
+		query.build(SQL.SELECT, "*", SQL.FROM, pdao.getDataObjectService().getView(ViewEnum.IHM_NORME), SQL.WHERE, "id_norme='PHASE3V1'");
 		Map<String, List<String>> viewNormSelectedRecords = new GenericBean(UtilitaireDao.get(0).executeRequest(c, query)).mapContent();
 		pdao.setSelectedRecords(viewNormSelectedRecords);
 		
@@ -99,7 +113,7 @@ public class GererNormeDaoTest extends InitializeQueryTest {
 		
 		// select the first record of viewCalendar and set it as the selected record
 		ArcPreparedStatementBuilder query = new ArcPreparedStatementBuilder();
-		query.build(SQL.SELECT, "*", SQL.FROM, pdao.getDataObjectService().getView(ViewEnum.IHM_CALENDRIER), SQL.WHERE, "id_norme='v2008-11'");
+		query.build(SQL.SELECT, "*", SQL.FROM, pdao.getDataObjectService().getView(ViewEnum.IHM_CALENDRIER), SQL.WHERE, "id_norme='PHASE3V1'");
 		Map<String, List<String>> viewCalendarSelectedRecords = new GenericBean(UtilitaireDao.get(0).executeRequest(c, query)).mapContent();
 		pdao.setSelectedRecords(viewCalendarSelectedRecords);
 		
@@ -126,7 +140,7 @@ public class GererNormeDaoTest extends InitializeQueryTest {
 		// select the first record of viewRulesSet and set it as the selected record
 		ArcPreparedStatementBuilder query = new ArcPreparedStatementBuilder();
 		query.build(SQL.SELECT, "*", SQL.FROM, pdao.getDataObjectService().getView(ViewEnum.IHM_JEUDEREGLE));
-		query.build(SQL.WHERE, "id_norme='v2008-11'", SQL.AND, "version='vConformite'");
+		query.build(SQL.WHERE, "id_norme='PHASE3V1'", SQL.AND, "version='v002'");
 		Map<String, List<String>> viewRulesSetSelectedRecords = new GenericBean(UtilitaireDao.get(0).executeRequest(c, query)).mapContent();
 		pdao.setSelectedRecords(viewRulesSetSelectedRecords);
 		
@@ -143,6 +157,49 @@ public class GererNormeDaoTest extends InitializeQueryTest {
 		// in test data, must return one loading rule set
 		assertEquals(1, viewChargement.getContent().t.size());
 
+	}
+	
+	@Test
+	public void uploadFileMapping() throws ArcException, IOException {
+		
+		String path = "src/test/resources/fr/insee/testfiles/ihm_mapping_regle_siera_test.csv";
+		File file = new File(path);
+	    byte[] fileBytes = Files.readAllBytes(file.toPath());
+	    MultipartFile multipartFile = new MockMultipartFile("file", file.getName(), "text/plain", fileBytes);
+
+		VObject viewMapping = new ViewMapping();
+		VObject viewRulesSet = new ViewJeuxDeRegles();
+		VObject viewNorme = new ViewNorme();
+		
+		
+		pdao.initializeViewRulesSet(viewRulesSet);
+		pdao.initializeViewNorme(viewNorme);	
+		viewMapping.setTable(pdao.getDataObjectService().getView(ViewEnum.IHM_MAPPING_REGLE));
+		
+		// select the first line of norme
+		viewNorme.setSelectedLines(Arrays.asList(true));
+		
+		// select the first line of ruleset
+		viewRulesSet.setSelectedLines(Arrays.asList(true));
+		
+		// execute query
+		pdao.uploadFileMapping(viewMapping, viewRulesSet, viewNorme, multipartFile);
+		
+		// test the content of ihm_mapping_regle
+		ArcPreparedStatementBuilder queryAssert = new ArcPreparedStatementBuilder();
+		queryAssert.build(SQL.SELECT, ColumnEnum.VARIABLE_SORTIE, ", ", ColumnEnum.EXPR_REGLE_COL, SQL.FROM, pdao.getDataObjectService().getView(ViewEnum.IHM_MAPPING_REGLE));
+		
+		
+		Map<String, List<String>> viewMappingUpload = new GenericBean(UtilitaireDao.get(0).executeRequest(c, queryAssert)).mapContent();
+		assertEquals(131, viewMappingUpload.get(ColumnEnum.VARIABLE_SORTIE.toString())
+				.size()); // number of variables
+
+		
+		assertEquals(3, viewMappingUpload.get(ColumnEnum.EXPR_REGLE_COL.toString())
+				.stream()
+				.filter(Objects::isNull)
+			    .collect(Collectors.toList())
+			    .size()); // number of metadata absent from the file
 	}
 
 }
