ae7dc4b733ff7a29d4da28128a442daa8d390efe
Léo Manelphe <leo.manelphe@insee.fr>
2024-02-20 13:50:00 +0100

    feat: create parent directories + listObjectsInDirectory options

--- arc-utils/src/main/java/fr/insee/arc/utils/minio/S3Template.java
+++ arc-utils/src/main/java/fr/insee/arc/utils/minio/S3Template.java
@@ -34,6 +34,8 @@ import okhttp3.OkHttpClient;
 
 public class S3Template {
 
+	private static final String EXISTS_FILE = ".exists";
+	
 	public S3Template(String s3ApiUri, String bucket, String accessKey, String secretKey) {
 		super();
 		this.s3ApiUri = s3ApiUri;
@@ -78,17 +80,20 @@ public class S3Template {
 	/**
 	 * Crée un nouveau répertoire dans le bucket à l'emplacement donné
 	 * 
-	 * @param path le chemin du répertoire à créer (emplacement et nom)
+	 * @param path le chemin du répertoire à créer
 	 * @throws ArcException
 	 */
 	public void createDirectory(String path) throws ArcException {
+		path += (path.endsWith("/") ? "" : "/"); // ajouté au path si manquant
+		int indexSecondLastSubstr = path.lastIndexOf("/", path.length() - 2); // index de l'avant dernier "/"
+		if (indexSecondLastSubstr > 0) {
+			createDirectory(path.substring(0, indexSecondLastSubstr + 1)); // crée les répertoires parents
+		}
 		try {
-			getMinioClient()
-					.putObject(PutObjectArgs.builder().bucket(bucket).object(path + (path.endsWith("/") ? "" : "/"))
-							.stream(new ByteArrayInputStream(new byte[] {}), 0, -1).build()); // répertoire
-			getMinioClient().putObject(
-					PutObjectArgs.builder().bucket(bucket).object(path + (path.endsWith("/") ? "" : "/") + ".exists")
-							.stream(new ByteArrayInputStream(new byte[] { 0x01 }), 1, -1).build()); // fichier .exists
+			if (!isExists(path)) {
+				getMinioClient().putObject(PutObjectArgs.builder().bucket(bucket).object(path + EXISTS_FILE)
+						.stream(new ByteArrayInputStream(new byte[] { 0x01 }), 1, -1).build()); // fichier .exists
+			}
 		} catch (InvalidKeyException | ErrorResponseException | InsufficientDataException | InternalException
 				| InvalidResponseException | NoSuchAlgorithmException | ServerException | XmlParserException
 				| IllegalArgumentException | IOException e) {
@@ -100,8 +105,8 @@ public class S3Template {
 	/**
 	 * Copie un objet d'un bucket dans un autre emplacement de ce bucket
 	 * 
-	 * @param pathFrom le chemin de l'objet à copier (emplacement et nom)
-	 * @param pathTo   le chemin de la copie (emplacement et nom)
+	 * @param pathFrom le chemin de l'objet à copier
+	 * @param pathTo   le chemin de la copie
 	 * @throws ArcException
 	 */
 	public void copy(String pathFrom, String pathTo) throws ArcException {
@@ -119,8 +124,8 @@ public class S3Template {
 	/**
 	 * Télécharge un objet d'un bucket vers un emplacement hors S3
 	 * 
-	 * @param pathFrom le chemin de l'objet à copier (emplacement et nom)
-	 * @param fileTo   le fichier téléchargé (emplacement et nom)
+	 * @param pathFrom le chemin de l'objet à copier
+	 * @param fileTo   le fichier téléchargé
 	 * @throws ArcException
 	 */
 	public void download(String pathFrom, File fileTo) throws ArcException {
@@ -138,8 +143,8 @@ public class S3Template {
 	/**
 	 * Uploade un fichier d'un emplacement hors S3 vers un emplacement du bucket
 	 * 
-	 * @param fileFrom le fichier à uploader (emplacement et nom)
-	 * @param pathTo   le chemin vers lequel uploader (emplacement et nom)
+	 * @param fileFrom le fichier à uploader
+	 * @param pathTo   le chemin vers lequel uploader
 	 * @throws ArcException
 	 */
 	public void upload(File fileFrom, String pathTo) throws ArcException {
@@ -157,7 +162,7 @@ public class S3Template {
 	/**
 	 * Supprime un objet d'un bucket
 	 * 
-	 * @param path le chemin de l'objet à supprimer (emplacement et nom)
+	 * @param path le chemin de l'objet à supprimer
 	 * @throws ArcException
 	 */
 	public void delete(String path) throws ArcException {
@@ -171,6 +176,12 @@ public class S3Template {
 		}
 	}
 
+	/**
+	 * Supprime une liste d'objets
+	 * 
+	 * @param paths le chemin des objets à supprimer (emplacements et noms)
+	 * @throws ArcException
+	 */
 	public void delete(List<String> paths) throws ArcException {
 		for (String path : paths) {
 			delete(path);
@@ -180,21 +191,21 @@ public class S3Template {
 	/**
 	 * Supprime un répertoire d'un bucket avec son contenu
 	 * 
-	 * @param path le chemin du répertoire à supprimer (emplacement et nom)
+	 * @param path le chemin du répertoire à supprimer
 	 * @throws ArcException
 	 */
 	public void deleteDirectory(String path) throws ArcException {
 		path += (path.endsWith("/") ? "" : "/");
 		delete(path);
-		delete(listObjectsInDirectory(path));
+		delete(listObjectsInDirectory(path, true, true, false));
 	}
 
 	/**
 	 * Déplace un objet d'un bucket vers un nouvel emplacement dans le même bucket.
 	 * Cette méthode peut être aussi utilisée pour renommer un objet.
 	 * 
-	 * @param pathFrom le chemin de l'objet à déplacer (emplacement et nom)
-	 * @param pathTo   le nouveau chemin de l'objet (emplacement et nom)
+	 * @param pathFrom le chemin de l'objet à déplacer
+	 * @param pathTo   le nouveau chemin de l'objet
 	 * @throws ArcException
 	 */
 	public void move(String pathFrom, String pathTo) throws ArcException {
@@ -205,12 +216,14 @@ public class S3Template {
 	/**
 	 * Renvoie la taille en octets d'un objet d'un bucket
 	 * 
-	 * @param path le chemin de l'objet à mesurer (emplacement et nom)
+	 * @param path le chemin de l'objet à mesurer
 	 * @return la taille en nombre d'octets de l'objet
 	 * @throws ArcException
 	 */
 	public long size(String path) throws ArcException {
-
+		if (isDirectory(path)) {
+			return size(listObjectsInDirectory(path, true, true, false));
+		}
 		StatObjectResponse statObject;
 		try {
 			statObject = getMinioClient().statObject(StatObjectArgs.builder().bucket(bucket).object(path).build());
@@ -224,6 +237,13 @@ public class S3Template {
 		return statObject.size();
 	}
 
+	/**
+	 * Renvoie la taille totale en octets d'une liste d'objets
+	 * 
+	 * @param paths le chemin des objets à mesurer (emplacements et noms)
+	 * @return la taille en nombre d'octets des objets
+	 * @throws ArcException
+	 */
 	public long size(List<String> paths) throws ArcException {
 		long sizeTotal = 0;
 		for (String path : paths) {
@@ -235,10 +255,9 @@ public class S3Template {
 	/**
 	 * Vérifie qu'un objet existe dans le bucket au chemin donné
 	 * 
-	 * @param path le chemin à vérifier (emplacement et nom)
+	 * @param path le chemin à vérifier
 	 * @return vrai si un objet existe avec ce chemin
 	 * @throws ArcException
-	 * @throws
 	 */
 	public boolean isExists(String path) throws ArcException {
 		@SuppressWarnings("unused")
@@ -246,7 +265,7 @@ public class S3Template {
 		boolean found;
 		try {
 			statObject = getMinioClient().statObject(StatObjectArgs.builder().bucket(bucket)
-					.object(path + (path.endsWith("/") ? ".exists" : "")).build());
+					.object(path + (path.endsWith("/") ? EXISTS_FILE : "")).build());
 			found = true;
 		} catch (ErrorResponseException e) {
 			if (e.response().code() != 404) {
@@ -262,13 +281,40 @@ public class S3Template {
 		return found;
 	}
 
-	public List<String> listObjectsInDirectory(String path) throws ArcException {
+	/**
+	 * Indique si le chemin en entrée est un répertoire qui existe.
+	 * 
+	 * @param path le chemin à tester
+	 * @return vrai si le chemin est celui d'un répertoire qui existe
+	 * @throws ArcException
+	 */
+	public boolean isDirectory(String path) throws ArcException {
+		return isExists(path) && path.endsWith("/");
+	}
+
+	/**
+	 * Liste l'ensemble des objets présents dans un répertoire
+	 * 
+	 * @param path           le chemin du répertoire
+	 * @param isRecursive    si true, inclut les objets des sous-répertoires
+	 * @param includeExists  si true, inclut les fichiers .exists
+	 * @param includeSubdirs si true, inclut les noms des sous-répertoires
+	 * @return la liste des objets du répertoire
+	 * @throws ArcException
+	 */
+	public List<String> listObjectsInDirectory(String path, Boolean isRecursive, Boolean includeExists,
+			Boolean includeSubdirs) throws ArcException {
 		List<String> listNames = new ArrayList<>();
 		Iterator<Result<Item>> listObject = getMinioClient()
-				.listObjects(ListObjectsArgs.builder().bucket(bucket).prefix(path).build()).iterator();
+				.listObjects(ListObjectsArgs.builder().bucket(bucket).prefix(path).recursive(isRecursive).build())
+				.iterator();
 		while (listObject.hasNext()) {
 			try {
-				listNames.add(listObject.next().get().objectName());
+				String nextObject = listObject.next().get().objectName();
+				if ((includeExists || !nextObject.endsWith(EXISTS_FILE))
+						&& (includeSubdirs || !nextObject.endsWith("/"))) {
+					listNames.add(nextObject);
+				}
 			} catch (InvalidKeyException | ErrorResponseException | IllegalArgumentException | InsufficientDataException
 					| InternalException | InvalidResponseException | NoSuchAlgorithmException | ServerException
 					| XmlParserException | IOException e) {
@@ -277,6 +323,18 @@ public class S3Template {
 			}
 		}
 		return listNames;
+	}
+
+	/**
+	 * Liste l'ensemble des objets présents dans un répertoire, en excluant le
+	 * contenu des sous-répertoires
+	 * 
+	 * @param path le chemin du répertoire
+	 * @return la liste des objets et sous-répertoires du répertoire
+	 * @throws ArcException
+	 */
+	public List<String> listObjectsInDirectory(String path) throws ArcException {
+		return listObjectsInDirectory(path, false, false, false);
 	}
 
 	public void closeMinioClient() {
