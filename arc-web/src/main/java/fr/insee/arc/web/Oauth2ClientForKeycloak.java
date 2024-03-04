a41f12ae8add9a85d08c3217322b661bd755f09f
Manuel Soulier <manuel.soulier@insee.fr>
2024-03-04 11:33:45 +0100

    feat: log keycloak user settings

--- arc-web/src/main/java/fr/insee/arc/web/Oauth2ClientForKeycloak.java
+++ arc-web/src/main/java/fr/insee/arc/web/Oauth2ClientForKeycloak.java
@@ -21,12 +21,14 @@ import fr.insee.arc.utils.webutils.WebAttributesName;
 
 public class Oauth2ClientForKeycloak {
 
+	private static final Logger LOGGER = LogManager.getLogger(Oauth2ClientForKeycloak.class);
+
 	@Value(WebAttributesName.KEYCLOAK_ATTRIBUTE_REALM)
 	protected String keycloakRealm;
 
 	@Value(WebAttributesName.KEYCLOAK_ATTRIBUTE_SERVER)
 	private String keycloakServer;
-
+	
 	@Value(WebAttributesName.KEYCLOAK_ATTRIBUTE_RESOURCE)
 	private String keycloakResource;
 
@@ -46,40 +48,37 @@ public class Oauth2ClientForKeycloak {
 				.clientAuthenticationMethod(method) //
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE) //
 				.issuerUri(realmUri) //
-				.scope("openid","profile","email","roles")
-				.authorizationUri(realmUri + openIdConnect + "/auth") //
+				.scope("openid", "profile", "email", "roles").authorizationUri(realmUri + openIdConnect + "/auth") //
 				.tokenUri(realmUri + openIdConnect + "/token") //
 				.userInfoUri(realmUri + openIdConnect + "/userinfo") //
 				.jwkSetUri(realmUri + openIdConnect + "/certs") //
 				.userNameAttributeName("preferred_username") //
 				.build();
 	}
-	
+
 	protected GrantedAuthoritiesMapper userAuthoritiesMapper() {
-	    return (authorities) -> {
-	      Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+		return (authorities) -> {
+			Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
 
-	      authorities.forEach(
-	          authority -> {
-	            if (authority instanceof OidcUserAuthority) {
-	              OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
-	              OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
-	              
-	              List<String> roles = userInfo.getClaimAsStringList("roles");
-	              if (roles==null)
-	              {
-	            	  roles = userInfo.getClaimAsStringList("groups");
-	              }
+			authorities.forEach(authority -> {
+				if (authority instanceof OidcUserAuthority) {
+					OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
+					OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
 
-	              List<SimpleGrantedAuthority> groupAuthorities =
-	            		  roles.stream()
-	                      .map(g -> new SimpleGrantedAuthority(g))
-	                      .toList();
-	              mappedAuthorities.addAll(groupAuthorities);
-	            }
-	          });
+					StaticLoggerDispatcher.custom(LOGGER, userInfo.getClaims());
 
-	      return mappedAuthorities;
-	    };
-	  }
+					List<String> roles = userInfo.getClaimAsStringList("roles");
+					if (roles == null) {
+						roles = userInfo.getClaimAsStringList("groups");
+					}
+
+					List<SimpleGrantedAuthority> groupAuthorities = roles.stream()
+							.map(g -> new SimpleGrantedAuthority(g)).toList();
+					mappedAuthorities.addAll(groupAuthorities);
+				}
+			});
+
+			return mappedAuthorities;
+		};
+	}
 }
