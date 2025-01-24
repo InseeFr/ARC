7c51a565877c4dc5fa4ce781d537a80249054588
Manuel Soulier <manuel.soulier@insee.fr>
2025-01-24 13:58:19 +0100

    fix: sonar name convention

--- arc-utils/src/test/java/fr/insee/arc/utils/utils/FormatSQLRenamingMethodsTest.java
+++ arc-utils/src/test/java/fr/insee/arc/utils/utils/FormatSQLRenamingMethodsTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class FormatSQLTestRenamingMethods {
+public class FormatSQLRenamingMethodsTest {
 	
 	@Test
 	public void temporaryTableNameTest()
