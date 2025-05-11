57c512602f028e05a3657d8b04e2c59202df4399
Manuel Soulier <manuel.soulier@insee.fr>
2025-05-11 22:14:02 +0200

    fix: code duplication

--- arc-core/src/main/java/fr/insee/arc/core/service/mutiphase/thread/ThreadMultiphaseService.java
+++ arc-core/src/main/java/fr/insee/arc/core/service/mutiphase/thread/ThreadMultiphaseService.java
@@ -10,6 +10,7 @@ import fr.insee.arc.core.dataobjects.ColumnEnum;
 import fr.insee.arc.core.model.TraitementPhase;
 import fr.insee.arc.core.service.global.scalability.ScalableConnection;
 import fr.insee.arc.core.service.global.thread.IThread;
+import fr.insee.arc.core.service.global.thread.ThreadTemplate;
 import fr.insee.arc.core.service.mutiphase.ApiMultiphaseService;
 import fr.insee.arc.core.service.p2chargement.thread.ThreadChargementService;
 import fr.insee.arc.core.service.p3normage.thread.ThreadNormageService;
@@ -17,7 +18,7 @@ import fr.insee.arc.core.service.p4controle.thread.ThreadControleService;
 import fr.insee.arc.core.service.p5mapping.thread.ThreadMappingService;
 import fr.insee.arc.core.util.StaticLoggerDispatcher;
 
-public class ThreadMultiphaseService extends ThreadMultiphaseTemplate implements Runnable, IThread<ApiMultiphaseService> {
+public class ThreadMultiphaseService extends ThreadTemplate implements Runnable, IThread<ApiMultiphaseService> {
 
 	private static final Logger LOGGER = LogManager.getLogger(ThreadMultiphaseService.class);
 
