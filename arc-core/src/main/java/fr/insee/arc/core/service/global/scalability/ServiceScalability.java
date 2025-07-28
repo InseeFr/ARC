271b3980e1183150e4e698c2c6d8f11790a8088b
Manuel Soulier <manuel.soulier@insee.fr>
2025-07-28 16:35:02 +0200

    feat: multithreading sync on nods

--- arc-core/src/main/java/fr/insee/arc/core/service/global/scalability/ServiceScalability.java
+++ arc-core/src/main/java/fr/insee/arc/core/service/global/scalability/ServiceScalability.java
@@ -1,16 +1,23 @@
 package fr.insee.arc.core.service.global.scalability;
 
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import fr.insee.arc.utils.consumer.ThrowingConsumer;
-import fr.insee.arc.utils.dao.UtilitaireDao;
 import fr.insee.arc.utils.database.ArcDatabase;
 import fr.insee.arc.utils.exception.ArcException;
 import fr.insee.arc.utils.exception.ArcExceptionMessage;
+import fr.insee.arc.utils.utils.LoggerHelper;
 
 public class ServiceScalability {
 
+	private static final Logger LOGGER = LogManager.getLogger(ServiceScalability.class);
+
+	
 	  private ServiceScalability() {
 		    throw new IllegalStateException("Utility class to dispatch a treatment over connections");
 		  }
@@ -27,42 +34,34 @@ public class ServiceScalability {
 	 */
 	public static int dispatchOnNods(Connection coordinatorConnexion, ThrowingConsumer<Connection> actionOnCoordinator, ThrowingConsumer<Connection> actionOnExecutor) throws ArcException
 	{
-		if (coordinatorConnexion==null)
-		{
-			try (Connection newCoordinatorConnexion = UtilitaireDao.get(ArcDatabase.COORDINATOR.getIndex()).getDriverConnexion())
-			{
-				actionOnCoordinator.accept(newCoordinatorConnexion);
-				
-			} catch (SQLException | ArcException e) {
-				 ArcException customException = new ArcException(e, ArcExceptionMessage.DATABASE_CONNECTION_COORDINATOR_FAILED);
-				 customException.logFullException();
-				 throw customException;
-			}
-		}
-		else
-		{
-			actionOnCoordinator.accept(coordinatorConnexion);
-		}
 		
+		List<Thread> threadPool = new ArrayList<Thread>();	
+		threadPool.add(new ThreadDispatchOnCoordinator(actionOnCoordinator, coordinatorConnexion));
+
 		// dispatch when scaled
 		int numberOfExecutorNods = ArcDatabase.numberOfExecutorNods();
-		if (numberOfExecutorNods==0)
+		
+		if (numberOfExecutorNods>0)
 		{
-			return numberOfExecutorNods;
+			for (int executorConnectionIndex=ArcDatabase.EXECUTOR.getIndex(); executorConnectionIndex<ArcDatabase.EXECUTOR.getIndex()+numberOfExecutorNods; executorConnectionIndex++ )
+			{
+				threadPool.add(new ThreadDispatchOnExecutor(actionOnExecutor, executorConnectionIndex));
+			}
 		}
 		
-		for (int executorConnectionIndex=ArcDatabase.EXECUTOR.getIndex(); executorConnectionIndex<ArcDatabase.EXECUTOR.getIndex()+numberOfExecutorNods; executorConnectionIndex++ )
+		// start threads
+		for (Thread t : threadPool)
 		{
-			
-			// instanciate connexion
-			try (Connection executorConnection = UtilitaireDao.get(executorConnectionIndex).getDriverConnexion())
-			{
-				actionOnExecutor.accept(executorConnection);
-
-			} catch (SQLException | ArcException e) {
-				 ArcException customException = new ArcException(e, ArcExceptionMessage.DATABASE_CONNECTION_EXECUTOR_FAILED);
-				 customException.logFullException();
-				 throw customException;
+			t.start();
+		}
+		
+		// join threads
+		for (Thread t : threadPool)
+		{
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				throw new ArcException(ArcExceptionMessage.MULTITHREADING_DISPATCH_FAILED);
 			}
 		}
 		
