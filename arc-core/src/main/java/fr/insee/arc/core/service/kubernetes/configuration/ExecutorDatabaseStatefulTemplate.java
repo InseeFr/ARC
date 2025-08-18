8afbc096423d4e464d2772446c1040aa10f3579f
Manuel Soulier <manuel.soulier@insee.fr>
2025-08-18 19:20:32 +0200

    fix: pguser in template and test

--- arc-core/src/main/java/fr/insee/arc/core/service/kubernetes/configuration/ExecutorDatabaseStatefulTemplate.java
+++ arc-core/src/main/java/fr/insee/arc/core/service/kubernetes/configuration/ExecutorDatabaseStatefulTemplate.java
@@ -96,7 +96,7 @@ public class ExecutorDatabaseStatefulTemplate {
                 "value": "{user}"
               },
               {
-                "name": "PGUSER ",
+                "name": "PGUSER",
                 "value": "{user}"
               },
               {
