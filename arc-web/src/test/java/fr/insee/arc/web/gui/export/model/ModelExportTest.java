aab165aab3272961d3f866b9c181852e18efef44
Manuel Soulier <manuel.soulier@insee.fr>
2025-08-25 18:38:12 +0200

    feat: test modelExport

---
+++ arc-web/src/test/java/fr/insee/arc/web/gui/export/model/ModelExportTest.java
@@ -0,0 +1,36 @@
+package fr.insee.arc.web.gui.export.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ModelExportTest {
+
+	@Test
+	public void modelExportGSTest() {
+		
+		ModelExport modelExport = new ModelExport();
+		
+		assertEquals("view.export",modelExport.getViewExport().getTitle());
+		assertEquals("view.exportOption",modelExport.getViewExportOption().getTitle());
+		assertEquals("view.exportFile",modelExport.getViewFileExport().getTitle());
+		
+		ViewExport v0 = new ViewExport();
+		v0.setTitle("t0");
+		modelExport.setViewExport(v0);
+		assertEquals("t0",modelExport.getViewExport().getTitle());
+
+		ViewExportOption v1 = new ViewExportOption();
+		v1.setTitle("t1");
+		modelExport.setViewExportOption(v1);
+		assertEquals("t1",modelExport.getViewExportOption().getTitle());
+		
+
+		ViewFileExport v2 = new ViewFileExport();
+		v2.setTitle("t2");
+		modelExport.setViewFileExport(v2);
+		assertEquals("t2",modelExport.getViewFileExport().getTitle());
+		
+	}
+
+}
