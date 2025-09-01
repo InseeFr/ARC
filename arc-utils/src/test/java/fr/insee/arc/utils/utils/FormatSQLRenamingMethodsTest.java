3355d76cf85e350bd49102af894e3d3d5dd16c88
Manuel Soulier <manuel.soulier@insee.fr>
2025-09-01 18:55:36 +0200

    fix: test FormatSQLRenamingMethodsTest

--- arc-utils/src/test/java/fr/insee/arc/utils/utils/FormatSQLRenamingMethodsTest.java
+++ arc-utils/src/test/java/fr/insee/arc/utils/utils/FormatSQLRenamingMethodsTest.java
@@ -33,8 +33,9 @@ public class FormatSQLRenamingMethodsTest {
 				FormatSQL.imageObjectName(tableName61Bytes));
 
 		System.out.println(FormatSQL.imageObjectName(tableName61Bytes, "000"));
-		assertEquals("7890123456789012345678901234567890123456789012345678901_img000",
+		assertEquals("67890123456789012345678901234567890123456789012345678901_img000",
 				FormatSQL.imageObjectName(tableName61Bytes, "000"));
+		
 		assertEquals(63,FormatSQL.imageObjectName(tableName61Bytes, "000").length());
 		
 	}
