6e8aa7faeb0b344b104268873addc337b124e36b
Manuel Soulier <manuel.soulier@insee.fr>
2025-09-03 16:07:26 +0200

    feat: use duckdb for data transfer between executors and master nod

--- arc-core/src/main/java/fr/insee/arc/core/service/p6export/dao/ExportMasterNodDao.java
+++ arc-core/src/main/java/fr/insee/arc/core/service/p6export/dao/ExportMasterNodDao.java
@@ -1,7 +1,6 @@
 package fr.insee.arc.core.service.p6export.dao;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,25 +8,27 @@ import fr.insee.arc.core.dataobjects.ArcPreparedStatementBuilder;
 import fr.insee.arc.core.dataobjects.ColumnEnum;
 import fr.insee.arc.core.dataobjects.ViewEnum;
 import fr.insee.arc.core.service.global.bo.Sandbox;
-import fr.insee.arc.core.service.global.scalability.ServiceScalability;
 import fr.insee.arc.utils.consumer.ThrowingConsumer;
-import fr.insee.arc.utils.dao.CopyObjectsToDatabase;
+import fr.insee.arc.utils.dao.DuckdbDao;
 import fr.insee.arc.utils.dao.GenericPreparedStatementBuilder;
 import fr.insee.arc.utils.dao.SQL;
 import fr.insee.arc.utils.dao.UtilitaireDao;
 import fr.insee.arc.utils.database.ArcDatabase;
+import fr.insee.arc.utils.dataobjects.ColumnAttributes;
 import fr.insee.arc.utils.exception.ArcException;
-import fr.insee.arc.utils.exception.ArcExceptionMessage;
 import fr.insee.arc.utils.structure.GenericBean;
 import fr.insee.arc.utils.utils.FormatSQL;
 
 public class ExportMasterNodDao {
 
 	private Sandbox coordinatorSandbox;
+	
+	private DuckdbDao duckdbDao;
 
 
 	public ExportMasterNodDao(Sandbox coordinatorSandbox) {
 		this.coordinatorSandbox = coordinatorSandbox;
+		this.duckdbDao = new DuckdbDao();
 	}
 	/**
 	 * select mapping tables found on the executor by listing table in
@@ -85,22 +86,11 @@ public class ExportMasterNodDao {
 	 */
 	public void copyMappingTablesToMasterNod(Set<String> mappingTablesName) throws ArcException {
 
-		// create link extension
-		CopyObjectsToDatabase.createExtensionDblink(coordinatorSandbox.getConnection());
-		
 		createImagesOfMappingTable(mappingTablesName);
 
-		try {
-			transfertDataFromExecutorNodsToMasterNodImageTables(mappingTablesName);
+		transfertDataFromExecutorNodsToMasterNodImageTables(mappingTablesName);
 			
-			copyImageTablesToParentMappingTables(mappingTablesName);
-			
-		}
-		finally
-		{
-			// always drop link extension at the end : musn't keep that for security concern
-			CopyObjectsToDatabase.dropExtensionDblink(coordinatorSandbox.getConnection());
-		}
+		copyImageTablesToParentMappingTables(mappingTablesName);
 
 	}
 
@@ -118,11 +108,7 @@ public class ExportMasterNodDao {
 			String tableName = ViewEnum.getFullName(coordinatorSandbox.getSchema(), mappingTableName);
 			String tableNameImage = FormatSQL.imageObjectName(tableName);
 			query.build(SQL.DROP, SQL.TABLE, SQL.IF_EXISTS, tableNameImage
-					, SQL.END_QUERY, SQL.BR);
-			query.build(SQL.CREATE, SQL.UNLOGGED, SQL.TABLE, tableNameImage, SQL.WITH, SQL.NO_VACUUM,
-				SQL.AS, SQL.SELECT, "*", SQL.FROM, tableName, SQL.WHERE, "false"
-				, SQL.END_QUERY, SQL.BR);
-			
+					, SQL.END_QUERY);			
 		}
 		UtilitaireDao.get(0).executeRequest(coordinatorSandbox.getConnection(), query);		
 	}
@@ -135,34 +121,23 @@ public class ExportMasterNodDao {
 	 */
 	private void transfertDataFromExecutorNodsToMasterNodImageTables(Set<String> mappingTablesName) throws ArcException {
 		
-		
-		ThrowingConsumer<Connection> functionCoordinator = coordinatorConnection -> 
-		{
-		};
-		
-		ThrowingConsumer<Connection> functionExecutor = executorConnection -> 
-		{
-			try (Connection coordinatorConnection = UtilitaireDao.get(ArcDatabase.COORDINATOR.getIndex()).getDriverConnexion())
+		ThrowingConsumer<Connection> transfert = duckdbConnection ->
 			{
-				// iterate table to copy
 				for (String mappingTableName : mappingTablesName)
 				{
 					String tableName = ViewEnum.getFullName(coordinatorSandbox.getSchema(), mappingTableName);
 					String tableNameImage = FormatSQL.imageObjectName(tableName);
-
-					CopyObjectsToDatabase.execCopyFromTableWithoutDroppingTargetTableNorDblinkExtension(executorConnection, coordinatorConnection, tableName, tableNameImage);
 					
-					// truncate table after copy to master nod
-					UtilitaireDao.get(0).executeRequest(executorConnection, FormatSQL.truncate(tableName));
+					ArcPreparedStatementBuilder query = new ArcPreparedStatementBuilder();
+					query.build(SQL.CREATE, SQL.TABLE, duckdbDao.attachedTableName(0, tableNameImage), SQL.AS);
+					query.append(duckdbDao.selectTableFromAllExecutorNods(tableName));
+					duckdbDao.executeQuery(duckdbConnection, query);
 				}
-			} catch (SQLException e) {
-				throw new ArcException(ArcExceptionMessage.DATABASE_CONNECTION_COORDINATOR_FAILED);
 			}
-		};
-		
-		// open dblink to exery executors nod and execute copy in parallel 
-		ServiceScalability.dispatchOnNods(coordinatorSandbox.getConnection(), functionCoordinator, functionExecutor);
-		
+			;
+
+			duckdbDao.executeOnDuckdb(transfert);
+
 	}
 	
 	
@@ -182,8 +157,12 @@ public class ExportMasterNodDao {
 		{
 			String tableName = ViewEnum.getFullName(coordinatorSandbox.getSchema(), mappingTableName);
 			String tableNameImage = FormatSQL.imageObjectName(tableName);
-			query.build(SQL.INSERT_INTO, tableName , SQL.SELECT, "*", SQL.FROM, tableNameImage, SQL.END_QUERY, SQL.BR);
-			query.build(SQL.DROP, SQL.TABLE, SQL.IF_EXISTS, tableNameImage, SQL.END_QUERY, SQL.BR);
+			
+			ColumnAttributes columnsOfImageTable = UtilitaireDao.get(0).retrieveColumnAttributesOfTable(coordinatorSandbox.getConnection(), tableNameImage);
+			
+			query.build(SQL.INSERT_INTO, tableName, "(", columnsOfImageTable.getCols(), ")" 
+					, SQL.SELECT, columnsOfImageTable.getCols(), SQL.FROM, tableNameImage, SQL.END_QUERY);
+			query.build(SQL.DROP, SQL.TABLE, SQL.IF_EXISTS, tableNameImage, SQL.END_QUERY);
 			
 		}
 		query.build(SQL.COMMIT, SQL.END_QUERY);
--- arc-core/src/main/java/fr/insee/arc/core/service/p6export/parquet/ParquetDao.java
+++ arc-core/src/main/java/fr/insee/arc/core/service/p6export/parquet/ParquetDao.java
@@ -1,34 +1,24 @@
 package fr.insee.arc.core.service.p6export.parquet;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Paths;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Properties;
 
-import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
-import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import fr.insee.arc.utils.consumer.ThrowingConsumer;
+import fr.insee.arc.utils.dao.DuckdbDao;
 import fr.insee.arc.utils.dao.GenericPreparedStatementBuilder;
 import fr.insee.arc.utils.dao.SQL;
 import fr.insee.arc.utils.database.ArcDatabase;
-import fr.insee.arc.utils.database.Delimiters;
 import fr.insee.arc.utils.database.TableToRetrieve;
 import fr.insee.arc.utils.exception.ArcException;
 import fr.insee.arc.utils.exception.ArcExceptionMessage;
-import fr.insee.arc.utils.files.CompressedUtils;
 import fr.insee.arc.utils.files.FileUtilsArc;
-import fr.insee.arc.utils.parquet.ParquetExtension;
-import fr.insee.arc.utils.ressourceUtils.ConnectionAttribute;
 import fr.insee.arc.utils.ressourceUtils.PropertiesHandler;
 import fr.insee.arc.utils.utils.FormatSQL;
 import fr.insee.arc.utils.utils.LoggerHelper;
@@ -37,23 +27,21 @@ public class ParquetDao {
 
 	private static final Logger LOGGER = LogManager.getLogger(ParquetDao.class);
 	
-	private static final String ATTACHMENT_NAME_PREFIX = "pg";
-
-	// classpath file containing extension files
-	private static final String DUCKDB_EXTENSION_PROVIDED_FILE = "duckdb/extensions.zip";
-
-	// directory where extension will be unzip and used by duckdb
-	private static final String DUCKDB_EXTENSION_INSTALLATION_DIRECTORY = Paths
-			.get(System.getProperty("java.io.tmpdir"), "duckdb").toString();
-
 	// parquet file format as "file.parquet"
 	private static final String PARQUET_FILE_EXTENSION = ".parquet";
 
-	ParquetEncryptionKey encryptionKey;
+	private ParquetEncryptionKey encryptionKey;
 	
-	boolean exportIfEmpty;
+	private boolean exportIfEmpty;
 	
+	protected DuckdbDao duckdbDao;
 	
+	public ParquetDao() {
+		super();
+		this.duckdbDao = new DuckdbDao();
+	}
+
+
 	/**
 	 * Export to parquet. Empty tables won't be exported.
 	 * @param tables
@@ -81,19 +69,9 @@ public class ParquetDao {
 		this.encryptionKey=encryptionKey;
 		this.exportIfEmpty=exportIfEmpty;
 		
-		// load duckdb extension
-		loadDuckdb();
-		
-		Properties connectionProperties = new Properties();
-		connectionProperties.setProperty("preserve_insertion_order", "false");
-		
-		try (Connection connection = DriverManager.getConnection("jdbc:duckdb:",connectionProperties)) {
-
-			// unzip extensions
-			unzipExtensions(connection);
-
-			// attach postgres database
-			attachPostgresDatabasesToDuckdb(connection);
+		ThrowingConsumer<Connection> exportToParquetOperation =  connection -> 
+		{
+			addParquetEncryptionKeyInDuckDb(connection);
 
 			// create output directory
 			FileUtilsArc.createDirIfNotexist(outputDirectory);
@@ -105,12 +83,26 @@ public class ParquetDao {
 				exportTableToParquet(connection, table, outputDirectory);
 				LoggerHelper.custom(LOGGER, "Parquet export end");
 			}
+		};
+		
+		duckdbDao.executeOnDuckdb(exportToParquetOperation);
+		
+	}
 
-		} catch (SQLException | IOException e) {
-			throw new ArcException(ArcExceptionMessage.SQL_EXECUTE_FAILED, e.getMessage());
+	private void addParquetEncryptionKeyInDuckDb(Connection connection) throws ArcException {
+
+		if (encryptionKey == null) {
+			return;
 		}
+		
+		GenericPreparedStatementBuilder query = new GenericPreparedStatementBuilder();
+		query.append("PRAGMA add_parquet_key(" + query.quoteText(encryptionKey.getType().getAlias()) + ","
+					+ query.quoteText(encryptionKey.getValue()) + ");");
+
+		duckdbDao.executeQuery(connection, query);		
 
 	}
+
 
 	/**
 	 * 
@@ -120,7 +112,7 @@ public class ParquetDao {
 	 * @throws SQLException
 	 */
 	private void exportTableToParquet(Connection connection, TableToRetrieve table, String outputDirectory)
-			throws SQLException {
+			throws ArcException {
 
 		String outputFileName = exportTablePath(table, outputDirectory);
 				
@@ -137,26 +129,14 @@ public class ParquetDao {
 	 * @param outputFileName
 	 * @throws SQLException
 	 */
-	private void exportExecutorTableToParquet(Connection connection, TableToRetrieve table, String outputFileName) throws SQLException {
+	private void exportExecutorTableToParquet(Connection connection, TableToRetrieve table, String outputFileName) throws ArcException {
 		
 		if (!table.getNod().equals(ArcDatabase.EXECUTOR)) {
 			return;
 		}
 		
-		PropertiesHandler properties = PropertiesHandler.getInstance();
-		
-		GenericPreparedStatementBuilder query = new GenericPreparedStatementBuilder();
-		boolean first = true;
-		for (int connectionIndex = ArcDatabase.EXECUTOR
-				.getIndex(); connectionIndex < properties.numberOfNods(); connectionIndex++) {
-			if (first) {
-				first = false;
-			} else {
-				query.append(SQL.UNION_ALL);
-			}
-			query.append("SELECT * FROM " + attachedTableName(connectionIndex, table.getTableName()));
-		}
-		
+		GenericPreparedStatementBuilder query = duckdbDao.selectTableFromAllExecutorNods(table.getTableName());
+
 		if (checkExportCondition(connection, query)) {
 			executeCopy(connection, query, outputFileName);
 		}
@@ -171,14 +151,14 @@ public class ParquetDao {
 	 * @throws SQLException
 	 */
 	private void exportCoordinatorTableToParquet(Connection connection, TableToRetrieve table, String outputFileName)
-			throws SQLException {
+			throws ArcException {
 
 		if (!table.getNod().equals(ArcDatabase.COORDINATOR)) {
 			return;
 		}
 
 		GenericPreparedStatementBuilder query = new GenericPreparedStatementBuilder();
-		query.append("SELECT * FROM " + attachedTableName(ArcDatabase.COORDINATOR.getIndex(), table.getTableName()));
+		query.append("SELECT * FROM " + duckdbDao.attachedTableName(ArcDatabase.COORDINATOR.getIndex(), table.getTableName()));
 		
 		if (checkExportCondition(connection, query)) {
 			executeCopy(connection, query, outputFileName);
@@ -191,8 +171,9 @@ public class ParquetDao {
 	 * @param query
 	 * @return
 	 * @throws SQLException
+	 * @throws ArcException 
 	 */
-	private boolean checkExportCondition(Connection connection, GenericPreparedStatementBuilder query) throws SQLException
+	private boolean checkExportCondition(Connection connection, GenericPreparedStatementBuilder query) throws ArcException
 	{
 		return this.exportIfEmpty || checkNotEmpty(connection, query);
 	}
@@ -206,7 +187,7 @@ public class ParquetDao {
 	 * @throws SQLException
 	 */
 	protected boolean checkNotEmpty(Connection connection, GenericPreparedStatementBuilder selectQuery)
-			throws SQLException {
+			throws ArcException {
 		GenericPreparedStatementBuilder query = new GenericPreparedStatementBuilder();
 		query.append("SELECT count(*) FROM (").append(selectQuery).append(" LIMIT 1) a;\n");
 		
@@ -220,6 +201,10 @@ public class ParquetDao {
 				countLine = rs.getInt(1);
 			}
 		}
+		catch (SQLException e)
+		{
+			throw new ArcException(ArcExceptionMessage.SQL_EXECUTE_FAILED, e.getMessage());
+		}
 		
 		return (countLine == 1);
 	}
@@ -232,7 +217,7 @@ public class ParquetDao {
 	 * @throws SQLException
 	 */
 	private void executeCopy(Connection connection, GenericPreparedStatementBuilder selectQuery, String output)
-			throws SQLException {
+			throws ArcException {
 		GenericPreparedStatementBuilder query = new GenericPreparedStatementBuilder();
 		query.append("COPY (").append(selectQuery).append(") TO " + query.quoteText(output));
 		
@@ -241,126 +226,7 @@ public class ParquetDao {
 		}
 		
 		query.append(";");
-		executeQuery(connection, query);
-	}
-
-	
-	/**
-	 * Attach the postgres databases (coordinator and executors) to duckdb engine
-	 * @param connection
-	 * @param encryptionKey
-	 * @throws SQLException
-	 * @throws IOException
-	 */
-	private void attachPostgresDatabasesToDuckdb(Connection connection)
-			throws SQLException {
-
-		PropertiesHandler properties = PropertiesHandler.getInstance();
-		
-		ConnectionAttribute[] postgresConnections = properties.connectionProperties().toArray(new ConnectionAttribute[0]);
-		
-		int numberOfPods = postgresConnections.length;
-
-		GenericPreparedStatementBuilder query = new GenericPreparedStatementBuilder();
-		
-		query.append("SET custom_extension_repository = " + query.quoteText(DUCKDB_EXTENSION_INSTALLATION_DIRECTORY)
-				+ ";\n");
-		query.append("SET extension_directory  = " + query.quoteText(DUCKDB_EXTENSION_INSTALLATION_DIRECTORY) + ";\n");
-		query.append("INSTALL postgres;\n");
-				
-		for (int connectionIndex = 0; connectionIndex < numberOfPods; connectionIndex++) {
-
-			ConnectionAttribute c = postgresConnections[connectionIndex];
-			String connexionChain = c.getConnectionChainInLibpqFormat();
-
-			query.append("ATTACH " + query.quoteText(connexionChain) + " AS " + attachmentName(connectionIndex)
-					+ " (TYPE postgres, READ_ONLY);\n");
-
-			if (encryptionKey != null) {
-				query.append("PRAGMA add_parquet_key(" + query.quoteText(encryptionKey.getType().getAlias()) + ","
-						+ query.quoteText(encryptionKey.getValue()) + ");");
-			}
-		}
-		executeQuery(connection, query);		
-	}
-
-	/**
-	 * unzip the duckdb postgres extension
-	 * @param connection 
-	 * @throws IOException
-	 * @throws SQLException 
-	 */
-	private void unzipExtensions(Connection connection) throws IOException, SQLException {
-
-		// check if temporary folder /temp/duckdb/version/ already exists
-		// if yes, do nothing : extensions have already been extracted
-		if (Paths.get(DUCKDB_EXTENSION_INSTALLATION_DIRECTORY, readDuckDbVersion(connection)).toFile().exists())
-		{
-			return;
-		}
-		
-		// unzip the extension file
-		try (InputStream is = ParquetExtension.class.getResourceAsStream(DUCKDB_EXTENSION_PROVIDED_FILE)) {
-			try (ZipArchiveInputStream zis = new ZipArchiveInputStream(is)) {
-				ZipArchiveEntry zae = zis.getNextEntry();
-				while (zae != null) {
-
-					// if already uncompressed, try next entry
-					if (new File(DUCKDB_EXTENSION_INSTALLATION_DIRECTORY + File.separator + zae).exists()) {
-						zae = zis.getNextEntry();
-						continue;
-					}
-
-					if (zae.isDirectory()) {
-						FileUtilsArc
-								.createDirIfNotexist(DUCKDB_EXTENSION_INSTALLATION_DIRECTORY + File.separator + zae);
-					} else {
-						try (FileOutputStream fos = new FileOutputStream(
-								DUCKDB_EXTENSION_INSTALLATION_DIRECTORY + File.separator + zae)) {
-							byte[] buffer = new byte[CompressedUtils.READ_BUFFER_SIZE];
-							int len;
-							while ((len = zis.read(buffer)) > 0) {
-								fos.write(buffer, 0, len);
-							}
-						}
-					}
-					zae = zis.getNextEntry();
-				}
-			}
-		}
-	}
-
-	private String readDuckDbVersion(Connection connection) throws SQLException {
-		String version;
-		try (PreparedStatement stmt = connection.prepareStatement("SELECT version()")) {
-			stmt.execute();
-			
-			try (ResultSet rs= stmt.getResultSet())
-			{
-				rs.next();
-				version = rs.getString(1);
-			}
-		}
-		return version;
-	}
-
-	private void executeQuery(Connection connection, GenericPreparedStatementBuilder query) throws SQLException {
-		try (PreparedStatement stmt = connection.prepareStatement(query.getQueryWithParameters())) {
-			stmt.execute();
-		}
-	}
-
-	/**
-	 * load duckdb driver
-	 * 
-	 * @throws ArcException
-	 */
-	private void loadDuckdb() throws ArcException {
-		try {
-			Class.forName("org.duckdb.DuckDBDriver");
-		} catch (ClassNotFoundException e) {
-			throw new ArcException(ArcExceptionMessage.DATABASE_CONNECTION_FAILED);
-		}
+		duckdbDao.executeQuery(connection, query);
 	}
 
 	
@@ -374,31 +240,6 @@ public class ParquetDao {
 	{
 		return outputDirectory + File.separator + FormatSQL.extractTableNameToken(table.getTableName())
 		+ PARQUET_FILE_EXTENSION;
-	}
-	
-	
-	/**
-	 * return the name of an table attached to a given postgre database duckdb use
-	 * database.schema.tablename format
-	 * 
-	 * @param tablename
-	 * @param connectionIndex
-	 * @return
-	 */
-	protected String attachedTableName(int connectionIndex, String tablename) {
-		return attachmentName(connectionIndex) + Delimiters.SQL_SCHEMA_DELIMITER + tablename;
-	}
-
-	/**
-	 * return a postgres attachment name for a given connection index as a reminder
-	 * connection index 0 is coordinator and 1+ are executors connection indexes
-	 * format return is pg_connectionIndex
-	 * 
-	 * @param connectionIndex
-	 * @return
-	 */
-	protected String attachmentName(int connectionIndex) {
-		return ATTACHMENT_NAME_PREFIX + Delimiters.SQL_TOKEN_DELIMITER + connectionIndex;
 	}
 
 }
