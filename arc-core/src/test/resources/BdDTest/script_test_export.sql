743d9c346a5c0dfe498f6f9f9b6927532868cb47
Manuel Soulier <manuel.soulier@insee.fr>
2026-04-16 11:13:38 +0200

    fix: export gui test

--- arc-core/src/test/resources/BdDTest/script_test_export.sql
+++ arc-core/src/test/resources/BdDTest/script_test_export.sql
@@ -1,6 +1,5 @@
 --
 -- Complète script_test_fonctionnel pour tester les requêtes d'export
 --
-
-INSERT INTO arc_bas1.export VALUES ('test1', 'export', NULL, NULL, NULL, NULL, NULL, 'x', 'x', NULL, '1');
-INSERT INTO arc_bas1.export VALUES ('test2', 'export', NULL, NULL, NULL, NULL, NULL, 'x', 'x', NULL, '2');
+INSERT INTO arc_bas1.export (file_name,table_to_export,nulls,headers,zip) VALUES ('test1', 'export', 'x', 'x', '1');
+INSERT INTO arc_bas1.export (file_name,table_to_export,nulls,headers,zip) VALUES ('test2', 'export', 'x', 'x', '2');
